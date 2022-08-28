c9ba1e07c8a19d383f8948fd3619db26b694a044
BJCHO0501 <bjcho1503@naver.com>
2022-08-28 21:29:33 +0900

    ✨ :: 프로젝트 생성

---
+++ Draw/Draw/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Draw
+//
+//  Created by 조병진 on 2022/08/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Draw/Draw/DrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawApp.swift
+//  Draw
+//
+//  Created by 조병진 on 2022/08/28.
+//
+
+import SwiftUI
+
+@main
+struct DrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

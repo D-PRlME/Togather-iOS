17220adab07ab70e82e34bd859483a8b08b62078
BJCHO0501 <bjcho1503@naver.com>
2022-08-31 17:48:41 +0900

    📝 :: Header

---
+++ Draw/Draw/Header/Header.swift
@@ -0,0 +1,59 @@
+import Foundation
+import ReactiveSwift
+
+
+struct Token {
+    static var _accessToken: String?
+    static var accessToken: String? {
+        get {
+            _accessToken =  UserDefaults.standard.string(forKey: "acccess_token")
+            return _accessToken
+        }
+        
+        set(newToken) {
+            UserDefaults.standard.set(newToken, forKey: "acccess_token")
+            _accessToken = newToken
+        }
+    }
+    
+    static var _refreshToken: String?
+    static var refreshToken: String? {
+        get {
+            _refreshToken = UserDefaults.standard.string(forKey: "refresh_token")
+            return _refreshToken
+        }
+        
+        set(newToken) {
+            UserDefaults.standard.set(newToken, forKey: "refresh_token")
+            _refreshToken = newToken
+        }
+    }
+    
+    static func removeToken() {
+        self.refreshToken = nil
+        self.accessToken = nil
+    }
+}
+
+enum Header {
+    case refreshToken, accessToken, tokenIsEmpty
+    
+    func header() -> [String : String]? {
+        guard let token = Token.accessToken else {
+            return ["Content-Type" : "application/json"]
+        }
+        
+        guard let refreshToken = Token.refreshToken else {
+            return ["Content-Type" : "application/json"]
+        }
+        
+        switch self {
+        case .refreshToken:
+            return ["Authorization" : "Bearer " + refreshToken, "Content-Type" : "application/json"]
+        case .accessToken:
+            return ["Authorization" : "Bearer " + token, "Content-Type" : "application/json"]
+        case .tokenIsEmpty:
+            return ["Content-Type" : "application/json"]
+        }
+    }
+}

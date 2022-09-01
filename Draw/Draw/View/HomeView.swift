8fd507bbc3fd88d808a30e70c38d5beca2971f66
hsj._.06 <gtw030488@gmail.com>
2022-09-01 23:01:55 +0900

    ⚡ :: SearchView 구상

--- Draw/Draw/View/HomeView.swift
+++ Draw/Draw/View/HomeView.swift
@@ -8,40 +8,37 @@ struct HomeView: View {
     
     var body: some View {
         GeometryReader { proxy in
-            ZStack {
+            ZStack() {
                 ColorManager.BackgroundColor1.ignoresSafeArea()
-                VStack {
-//                    Spacer()
-//                        .frame(height: 100)
-                    ScrollView(.horizontal, showsIndicators: true) {
-                        HStack {
-                            ForEach(0..<DevLanguage.count, id: \.self) { i in
-                                VStack {
-                                    Rectangle()
-                                        .foregroundColor(DevColor[i])
-                                        .frame(width: 56, height: 56)
-                                        .cornerRadius(20)
-                                        .padding(.horizontal, 8)
-                                    Text(DevLanguage[i])
-                                        .font(.custom("Pretendard-ExtraBold", size: 14))
-                                        .padding(.bottom, 9)
+                VStack(spacing: 0) {
+                    ScrollView() {
+                        ScrollView(.horizontal, showsIndicators: true) {
+                            HStack {
+                                ForEach(0..<DevLanguage.count, id: \.self) { i in
+                                    VStack {
+                                        Rectangle()
+                                            .foregroundColor(DevColor[i])
+                                            .frame(width: 56, height: 56)
+                                            .cornerRadius(20)
+                                            .padding(.horizontal, 8)
+                                        Text(DevLanguage[i])
+                                            .font(.custom("Pretendard-ExtraBold", size: 14))
+                                            .padding(.bottom, 9)
+                                    }
                                 }
                             }
+                            .padding(.horizontal,  8)
                         }
-                        .padding(.horizontal,  8)
-                    }
-                    Rectangle()
-                        .foregroundColor(Color("TabBarStroke"))
-                        .frame(width: proxy.size.width, height: 2)
-                    
-                    VStack(spacing: 0) {
-                        ScrollView() {
-                            ForEach(0..<6, id: \.self) { i in
-                                VStack(alignment: .leading, spacing: 8) {
-                                    Text("제목제목제목제목제목제목제목제목제목제목제목")
-                                        .font(.custom("Pretendard-Bold", size: 24))
-                                    HStack {
-                                        ForEach(0..<3, id: \.self) {_ in
+                        Rectangle()
+                            .foregroundColor(Color("TabBarStroke"))
+                            .frame(width: proxy.size.width, height: 2)
+                        ForEach(0..<6, id: \.self) { i in
+                            VStack(alignment: .leading, spacing: 8) {
+                                Text("제목제목제목제목제목제목제목제목제목제목제목")
+                                    .font(.custom("Pretendard-Bold", size: 24))
+                                HStack {
+                                    ForEach(0..<3, id: \.self) { _ in
+                                        VStack {
                                             ZStack {
                                                 Text(DevLanguage[i])
                                                     .padding(.horizontal, 13)
@@ -58,36 +55,38 @@ struct HomeView: View {
                                             }
                                         }
                                     }
-                                    Rectangle()
-                                        .foregroundColor(Color("TabBarStroke"))
-                                        .frame(width: proxy.size.width - 70, height: 2)
-                                    HStack(spacing: 8) {
-                                        Image(systemName: "person.fill")
-                                            .frame(width: 33, height: 33)
-                                            .overlay(Circle().stroke().foregroundColor(Color("TabBarStroke")))
-                                        Text("사람김")
-                                            .font(.custom("Pretendard-Medium", size: 16))
-                                        Spacer()
-                                        Text("1시간 전")
-                                            .font(.custom("Pretendard-Medium", size: 16))
-                                    }
                                 }
-                                .padding(12)
-                                .background(Color(red: 0.97, green: 0.97, blue: 0.97))
-                                .cornerRadius(8)
+                                Rectangle()
+                                    .foregroundColor(Color("TabBarStroke"))
+                                    .frame(width: proxy.size.width - 70, height: 2)
+                                HStack(spacing: 8) {
+                                    Image(systemName: "person.fill")
+                                        .frame(width: 33, height: 33)
+                                        .overlay(Circle().stroke().foregroundColor(Color("TabBarStroke")))
+                                    Text("사람김")
+                                        .font(.custom("Pretendard-Medium", size: 16))
+                                    Spacer()
+                                    Text("1시간 전")
+                                        .font(.custom("Pretendard-Medium", size: 16))
+                                }
                             }
-                            .padding(16)
-                            
+                            .padding(12)
+                            .background(Color(red: 0.97, green: 0.97, blue: 0.97))
+                            .cornerRadius(8)
                         }
-                        .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom == 0 ? 86 : 106)
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 8)
                         
                     }
-                    Spacer()
+                    .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom == 0 ? 86 : 106)
+                    
                 }
+                Spacer()
             }
         }
     }
 }
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
--- Draw/Draw/View/SearchView.swift
+++ Draw/Draw/View/SearchView.swift
@@ -1,5 +1,36 @@
 import SwiftUI
 
+struct SearchBar: View {
+    
+    @Binding var text: String
+ 
+    var body: some View {
+        HStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+ 
+                TextField("Search", text: $text)
+                    .foregroundColor(.primary)
+ 
+                if !text.isEmpty {
+                    Button(action: {
+                        self.text = ""
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                    }
+                } else {
+                    EmptyView()
+                }
+            }
+            .padding(EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 8))
+            .foregroundColor(.secondary)
+            .background(Color(.secondarySystemBackground))
+            .cornerRadius(10.0)
+        }
+        .padding(.horizontal)
+    }
+}
+
 struct SearchView: View {
     var body: some View {
         ZStack {

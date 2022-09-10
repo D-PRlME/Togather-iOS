708319548dcd6b33e000aa46d2e9920e00a1ae1b
hsj._.06 <gtw030488@gmail.com>
2022-09-10 19:55:31 +0900

    📝 :: 글작성 UI
    글작성 UI를 만들었습니다.

---
+++ Draw/Draw/View/EditInfo/DeleteAccount.swift
@@ -0,0 +1,20 @@
+//
+//  DeleteAccount.swift
+//  Draw
+//
+//  Created by 홍승재 on 2022/09/10.
+//
+
+import SwiftUI
+
+struct DeleteAccount: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DeleteAccount_Previews: PreviewProvider {
+    static var previews: some View {
+        DeleteAccount()
+    }
+}

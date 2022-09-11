fe1af59f6eaffab37202539445d0acafeb157cc4
hsj._.06 <gtw030488@gmail.com>
2022-09-11 17:59:15 +0900

    📝 :: 개발자들 UI
    개발자들 UI를 만들었다.

--- Draw/Draw/View/myview ->/Developers.swift
+++ Draw/Draw/View/myview ->/Developers.swift
@@ -8,8 +8,88 @@
 import SwiftUI
 
 struct Developers: View {
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    let columns = [
+        GridItem(.flexible(), spacing: 12),
+        GridItem(.flexible(), spacing: 12)
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { proxy in
+            ZStack {
+                ColorManager.BackgroundColor1.ignoresSafeArea()
+                VStack {
+                    HStack {
+                        Spacer()
+                        Image("CloseBtn")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .padding(.top, 16)
+                            .onTapGesture {
+                                self.presentationMode.wrappedValue.dismiss()
+                            }
+                    }
+                    ScrollView(.vertical, showsIndicators: false) {
+                        HStack {
+                            Text("개발자들")
+                                .foregroundColor(.black)
+                                .font(.custom("Pretendard-Bold", size: 28))
+                                .padding(.top, 40)
+                            Spacer()
+                        }
+                        LazyVGrid(columns: columns, spacing: 12) {
+                            ForEach(0..<Developer.count, id: \.self) { i in
+                                ZStack{
+                                    VStack(spacing: 8) {
+                                        HStack(spacing: 0) {
+                                            Circle()
+                                                .foregroundColor(DevColor[i])
+                                                .frame(width: 62, height: 62)
+                                            Spacer()
+                                            Text(Developer[i])
+                                                .foregroundColor(.black)
+                                                .font(.custom("Pretendard-Bold", size: 24))
+                                                .padding(.trailing, 8)
+                                        }
+                                        HStack {
+                                            Text("기술")
+                                                .foregroundColor(.black)
+                                                .font(.custom("Pretendard-Medium", size: 16))
+                                                .padding(.horizontal, 12)
+                                                .padding(.vertical, 6)
+                                                .background(Color(red: 0.905, green: 0.905, blue: 0.905))
+                                                .cornerRadius(37)
+                                                .padding(1)
+                                                .background(Color("TabBarStroke"))
+                                                .cornerRadius(37)
+                                            Spacer()
+                                        }
+                                        HStack(spacing: 0) {
+                                            Image("LinkImage")
+                                                .resizable()
+                                                .frame(width: 12, height: 12)
+                                                .padding(4)
+                                            Text("Github")
+                                                .foregroundColor(.black)
+                                                .font(.custom("Pretendard-Medium", size: 18))
+                                                .underline()
+                                            Spacer()
+                                        }
+                                    }
+                                }
+                                .padding(12)
+                                .background(Color("TabBarFill"))
+                                .cornerRadius(8)
+                            }
+                        }
+                    }
+                    Spacer()
+                }
+                .padding(.horizontal, 16)
+            }
+        }
     }
 }
 

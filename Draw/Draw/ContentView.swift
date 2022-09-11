adf9064d1b30037d863cd24d3dde95172988c544
hsj._.06 <gtw030488@gmail.com>
2022-09-12 02:01:58 +0900

    📝 :: SignInView UI
    로그인 뷰를 만들었다.

--- Draw/Draw/ContentView.swift
+++ Draw/Draw/ContentView.swift
@@ -12,7 +12,7 @@ struct ColorManager {
 
 struct ContentView: View {
     var body: some View {
-        TabBarView()
+        SignInView()
     }
 }
 
---
+++ Draw/Draw/View/SignInView.swift
@@ -0,0 +1,84 @@
+//
+//  SignInView.swift
+//  Draw
+//
+//  Created by 홍승재 on 2022/09/12.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        GeometryReader { proxy in
+            ZStack {
+                ColorManager.BackgroundColor1.ignoresSafeArea()
+                VStack(alignment: .leading, spacing: 12) {
+                    Spacer()
+                    VStack(alignment: .leading) {
+                        Text("마음 맞는 팀원을 찾는\n간단한 방법.")
+                            .foregroundColor(.black)
+                            .font(.custom("Pretendard-Bold", size: 32))
+                            .multilineTextAlignment(.leading)
+                        Text("Togather")
+                            .foregroundColor(Color("TabBarSelectedColor"))
+                            .font(.custom("Pretendard-Bold", size: 32))
+                    }
+                    TextField("이메일", text: $email)
+                        .font(.custom("Pretendard-Medium", size: 20))
+                        .foregroundColor(.black)
+                        .padding(12)
+                        .background(Color(red: 0.97, green: 0.97, blue: 0.97))
+                        .cornerRadius(6)
+                        .padding(1)
+                        .background(Color("TabBarStroke"))
+                        .cornerRadius(6)
+                    SecureField("비밀번호", text: $password)
+                        .font(.custom("Pretendard-Medium", size: 20))
+                        .foregroundColor(.black)
+                        .padding(12)
+                        .background(Color(red: 0.97, green: 0.97, blue: 0.97))
+                        .cornerRadius(6)
+                        .padding(1)
+                        .background(Color("TabBarStroke"))
+                        .cornerRadius(6)
+                    HStack(spacing: 12) {
+                        Spacer()
+                        Text("회원가입")
+                            .foregroundColor(Color(red: 0, green: 0, blue: 0, opacity: 0.5))
+                            .font(.custom("Pretendard-Medium", size: 18))
+                            .padding(.vertical, 13)
+                        Spacer()
+                        Text("비밀번호 변경")
+                            .foregroundColor(Color(red: 0, green: 0, blue: 0, opacity: 0.5))
+                            .font(.custom("Pretendard-Medium", size: 18))
+                            .padding(.vertical, 13)
+                        Spacer()
+                    }
+                    Spacer()
+                    HStack(spacing: 12) {
+                        Spacer()
+                        Image("GoogleLogo")
+                            .resizable()
+                            .frame(width: 48, height: 48)
+                        Image("AppleLogo")
+                            .resizable()
+                            .frame(width: 48, height: 48)
+                        Spacer()
+                    }
+                }
+            }
+            .padding(.horizontal, 16)
+            .padding(.bottom, 16)
+        }
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}

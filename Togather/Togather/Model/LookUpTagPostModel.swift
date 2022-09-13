d3649f8a62e9cfeb92dcb4ec404efb52c1294e35
hsj._.06 <gtw030488@gmail.com>
2022-09-13 15:51:34 +0900

    📝 :: LookUpTagPostModel

---
+++ Togather/Togather/Model/LookUpTagPostModel.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+// MARK: - LookUpTagPostModel (태그 게시글 목록 조회)
+struct LookUpTagPostModel: Codable {
+    let title:String
+    let userName: String
+    let tags: [String]
+    let createAt: String
+}
+
+/*
+{
+        "title":"프로젝트 백엔드 모집합니다",
+        "userName":"김성원",
+        "tags":[
+            "BACKEND",
+            "SPRING"
+        ],
+        "createAt":"몇시몇분몇초"
+}
+*/

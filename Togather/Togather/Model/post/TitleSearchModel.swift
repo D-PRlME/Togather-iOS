dd400076dfdf42dcbeda24815853a3eae3777628
hsj._.06 <gtw030488@gmail.com>
2022-09-15 17:08:54 +0900

    📝 :: TitleSearchModel

---
+++ Togather/Togather/Model/post/TitleSearchModel.swift
@@ -0,0 +1,20 @@
+//
+//  TitleSearchModel.swift
+//  Togather
+//
+//  Created by 홍승재 on 2022/09/15.
+//
+
+import SwiftUI
+
+struct TitleSearchModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TitleSearchModel_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleSearchModel()
+    }
+}

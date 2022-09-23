52a0a9a989d6bc115489a2ab48e65bd289082d09
BJCHO0501 <bjcho1503@naver.com>
2022-09-23 18:27:07 +0900

    📝 :: MyProfileModel 수정

--- Togather/Togather/Model/user/MyProfileModel.swift
+++ Togather/Togather/Model/user/MyProfileModel.swift
@@ -2,14 +2,14 @@ import Foundation
 
 // MARK: - MyProfileModel (내 프로필 조회)
 struct MyProfileModel: Codable {
-    let account_id: String
+    let name: String
     let email: String
     let profile_image_url: String
 }
 
 /*
  {
-    "account_id" : "qweqwe",
+    "name" : "qweqwe",
     "email" : "email.dsm.hs.kr",
     "profile_image_url" : "sasdlfkgjaglvsla.jpg"
  }

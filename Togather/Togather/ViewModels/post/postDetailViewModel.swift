1fa98da29666d87d816f797971affc318f320d47
hsj._.06 <gtw030488@gmail.com>
2022-09-23 18:58:17 +0900

    📝 :: 자잘한 파일 이동

--- Togather/Togather/ViewModels/post/postDetailViewModel.swift
+++ Togather/Togather/ViewModels/post/postDetailViewModel.swift
@@ -15,7 +15,9 @@ class PostDetailViewModel: ObservableObject {
         ],
         is_mine: false,
         content: "",
-        link: ""
+        link: "",
+        like_count: 0,
+        is_liked: false
     )
     
     func getPostDetail() {
@@ -40,6 +42,8 @@ class PostDetailViewModel: ObservableObject {
                         let is_mine = data.is_mine
                         let content = data.content
                         let link = data.link
+                        let like_count = data.like_count
+                        let is_liked = data.is_liked
                         
                         self.postDetail = Posts_Detail(
                             title: title,
@@ -48,7 +52,9 @@ class PostDetailViewModel: ObservableObject {
                             tags: tags,
                             is_mine: is_mine,
                             content: content,
-                            link: link
+                            link: link,
+                            like_count: like_count,
+                            is_liked: is_liked
                         )
                         print("게시물 상세 정보 : ", self.postDetail)
                         self.tagName = tags.map {
@@ -65,6 +71,73 @@ class PostDetailViewModel: ObservableObject {
                         print("message: \(data.message)")
                     } else {
                         print("⚠️post Error handling")
+                    }
+                }
+            case .failure(let err):
+                print("⛔️post error: \(err.localizedDescription)")
+            }
+        }
+    }
+    func like() {
+        postClient.request(.like(postID: postID)) { res in
+            switch res {
+            case .success(let result):
+                switch result.statusCode {
+                case 200...206:
+                    print("좋아요 성공!")
+                default:
+                    let decoder = JSONDecoder()
+                    if let data = try? decoder.decode(ErrorModel.self, from: result.data) {
+                        print("status: \(data.status)")
+                        print("code: \(data.code)")
+                        print("message: \(data.message)")
+                    } else {
+                        print("⚠️like Error handling")
+                    }
+                }
+            case .failure(let err):
+                print("⛔️post error: \(err.localizedDescription)")
+            }
+        }
+    }
+    func dislike() {
+        postClient.request(.dislike(postID: postID)) { res in
+            switch res {
+            case .success(let result):
+                switch result.statusCode {
+                case 200...206:
+                    print("좋아요 취소 성공!")
+                default:
+                    let decoder = JSONDecoder()
+                    if let data = try? decoder.decode(ErrorModel.self, from: result.data) {
+                        print("status: \(data.status)")
+                        print("code: \(data.code)")
+                        print("message: \(data.message)")
+                    } else {
+                        print("⚠️dislike Error handling")
+                    }
+                }
+            case .failure(let err):
+                print("⛔️post error: \(err.localizedDescription)")
+            }
+        }
+    }
+    
+    func delete() {
+        postClient.request(.deletePost(postID: postID)) { res in
+            switch res {
+            case .success(let result):
+                switch result.statusCode {
+                case 200...206:
+                    print("삭제 성공!")
+                default:
+                    let decoder = JSONDecoder()
+                    if let data = try? decoder.decode(ErrorModel.self, from: result.data) {
+                        print("status: \(data.status)")
+                        print("code: \(data.code)")
+                        print("message: \(data.message)")
+                    } else {
+                        print("⚠️Delete post Error handling")
                     }
                 }
             case .failure(let err):

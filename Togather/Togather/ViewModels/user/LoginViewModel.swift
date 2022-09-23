bcd1c6f87db9dce7d45bf05e7d955e64c74f7916
BJCHO0501 <bjcho1503@naver.com>
2022-09-23 18:26:15 +0900

    📝 :: ViewModel 수정

---
+++ Togather/Togather/ViewModels/user/LoginViewModel.swift
@@ -0,0 +1,47 @@
+import Foundation
+import Moya
+
+class LoginViewModel: ObservableObject {
+    let userClient = MoyaProvider<UserService>(plugins: [MoyaLoggerPlugin()])
+    
+    @Published var email: String = ""
+    @Published var password: String = ""
+    
+    @Published var viewTag: Int? = nil
+    
+    func Login() {
+        userClient.request(.login(accountID: email, password: password)) { res in
+            switch res {
+            case .success(let result):
+                switch result.statusCode {
+                case 200...201:
+                    let decoder = JSONDecoder()
+                    if let data = try? decoder.decode(TokenModel.self, from: result.data) {
+                        
+                        Token.accessToken = data.access_token
+                        Token.refreshToken = data.refresh_token
+                        
+                        print("✅로그인 성공")
+                        print("🔊\(data.expired_at)")
+                        self.viewTag = 1
+                    } else {
+                        print("⚠️login docoder error")
+                    }
+                default:
+                    let decoder = JSONDecoder()
+                    if let data = try? decoder.decode(ErrorModel.self, from: result.data) {
+                        print("============🆘============")
+                        print("status: \(data.status)")
+                        print("code: \(data.code)")
+                        print("message: \(data.message)")
+                        print("==========================")
+                    } else {
+                        print("⚠️login Error handling")
+                    }
+                }
+            case .failure(let err):
+                print("⛔️login error: \(err.localizedDescription)")
+            }
+        }
+    }
+}

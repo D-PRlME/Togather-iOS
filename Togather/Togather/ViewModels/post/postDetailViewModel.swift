a33014e82694fdc5bd539124ce322710e77ec7ef
BJCHO0501 <bjcho1503@naver.com>
2022-09-26 23:36:18 +0900

    📝 :: PostDetailViewModel

--- Togather/Togather/ViewModels/post/postDetailViewModel.swift
+++ Togather/Togather/ViewModels/post/postDetailViewModel.swift
@@ -28,56 +28,52 @@ class PostDetailViewModel: ObservableObject {
             case .success(let result):
                 switch result.statusCode {
                 case 200...206:
-                    let decoder = JSONDecoder()
-                    if let data = try? decoder.decode(PostDetailModel.self, from: result.data) {
-                        let title = data.title
-                        let users: Users = Users(user_id: data.user.user_id, user_name: data.user.user_name, profile_image_url: data.user.profile_image_url)
-                        let createdAt = data.created_at
-                        let tags: [Tags] = data.tags.map {
-                            let name = $0.name
-                            let imageURL = $0.image_url
+                    DispatchQueue.main.async {
+                        let decoder = JSONDecoder()
+                        if let data = try? decoder.decode(PostDetailModel.self, from: result.data) {
+                            let title = data.title
+                            let users: Users = Users(user_id: data.user.user_id, user_name: data.user.user_name, profile_image_url: data.user.profile_image_url)
+                            let createdAt = data.created_at
+                            let tags: [Tags] = data.tags.map {
+                                let name = $0.name
+                                let imageURL = $0.image_url
+                                
+                                return Tags(name: name, image_url: imageURL)
+                            }
+                            let is_mine = data.is_mine
+                            let content = data.content
+                            let link = data.link
+                            let like_count = data.like_count
+                            let is_liked = data.is_liked
                             
-                            return Tags(name: name, image_url: imageURL)
+                            self.postDetail = Posts_Detail(
+                                title: title,
+                                user: users,
+                                created_at: createdAt,
+                                tags: tags,
+                                is_mine: is_mine,
+                                content: content,
+                                link: link,
+                                like_count: like_count,
+                                is_liked: is_liked
+                            )
+                            print("게시물 상세 정보 : ", self.postDetail)
+                            self.tagName = tags.map {
+                                $0.name
+                            }
+                        } else {
+                            print("⚠️post detail docoder error")
                         }
-                        let is_mine = data.is_mine
-                        let content = data.content
-                        let link = data.link
-                        let like_count = data.like_count
-                        let is_liked = data.is_liked
-                        
-                        self.postDetail = Posts_Detail(
-                            title: title,
-                            user: users,
-                            created_at: createdAt,
-                            tags: tags,
-                            is_mine: is_mine,
-                            content: content,
-                            link: link,
-                            like_count: like_count,
-                            is_liked: is_liked
-                        )
-                        print("게시물 상세 정보 : ", self.postDetail)
-                        self.tagName = tags.map {
-                            $0.name
-                        }
-                    } else {
-                        print("⚠️post detail docoder error")
                     }
                 default:
-                    let decoder = JSONDecoder()
-                    if let data = try? decoder.decode(ErrorModel.self, from: result.data) {
-                        print("status: \(data.status)")
-                        print("code: \(data.code)")
-                        print("message: \(data.message)")
-                    } else {
-                        print("⚠️post Error handling")
-                    }
+                    print(result.statusCode)
                 }
             case .failure(let err):
                 print("⛔️post error: \(err.localizedDescription)")
             }
         }
     }
+    
     func like() {
         postClient.request(.like(postID: postID)) { res in
             switch res {
@@ -86,20 +82,14 @@ class PostDetailViewModel: ObservableObject {
                 case 200...206:
                     print("좋아요 성공!")
                 default:
-                    let decoder = JSONDecoder()
-                    if let data = try? decoder.decode(ErrorModel.self, from: result.data) {
-                        print("status: \(data.status)")
-                        print("code: \(data.code)")
-                        print("message: \(data.message)")
-                    } else {
-                        print("⚠️like Error handling")
-                    }
+                    print(result.statusCode)
                 }
             case .failure(let err):
                 print("⛔️post error: \(err.localizedDescription)")
             }
         }
     }
+    
     func dislike() {
         postClient.request(.dislike(postID: postID)) { res in
             switch res {
@@ -108,14 +98,7 @@ class PostDetailViewModel: ObservableObject {
                 case 200...206:
                     print("좋아요 취소 성공!")
                 default:
-                    let decoder = JSONDecoder()
-                    if let data = try? decoder.decode(ErrorModel.self, from: result.data) {
-                        print("status: \(data.status)")
-                        print("code: \(data.code)")
-                        print("message: \(data.message)")
-                    } else {
-                        print("⚠️dislike Error handling")
-                    }
+                    print(result.statusCode)
                 }
             case .failure(let err):
                 print("⛔️post error: \(err.localizedDescription)")
@@ -131,14 +114,7 @@ class PostDetailViewModel: ObservableObject {
                 case 200...206:
                     print("삭제 성공!")
                 default:
-                    let decoder = JSONDecoder()
-                    if let data = try? decoder.decode(ErrorModel.self, from: result.data) {
-                        print("status: \(data.status)")
-                        print("code: \(data.code)")
-                        print("message: \(data.message)")
-                    } else {
-                        print("⚠️Delete post Error handling")
-                    }
+                    print(result.statusCode)
                 }
             case .failure(let err):
                 print("⛔️post error: \(err.localizedDescription)")

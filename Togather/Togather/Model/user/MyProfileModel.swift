05b27b73f64b62879292b77cb74efe842ddc82b7
BJCHO0501 <bjcho1503@naver.com>
2022-10-07 16:06:43 +0900

    📝 :: MyProfileModel

--- Togather/Togather/Model/user/MyProfileModel.swift
+++ Togather/Togather/Model/user/MyProfileModel.swift
@@ -5,12 +5,16 @@ struct MyProfileModel: Codable {
     let name: String
     let email: String
     let profile_image_url: String
+    let introduce: String
+    let positions: [String]
 }
 
 /*
  {
-    "name" : "qweqwe",
-    "email" : "email.dsm.hs.kr",
-    "profile_image_url" : "sasdlfkgjaglvsla.jpg"
+     "name" : "김은빈",
+     "email" : "email@dsm.hs.kr",
+     "profile_image_url" : "http://",
+     "introduce":"백엔드 일짱임. 디코 아이디는 김은빈#1234",
+     "position":["BACKEND"]
  }
 */

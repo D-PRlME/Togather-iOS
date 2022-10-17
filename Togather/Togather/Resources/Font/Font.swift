b143dd4888d92a3f1e3e6bced85e6469edb2d65e
BJCHO0501 <bjcho1503@naver.com>
2022-10-17 14:54:08 +0900

    📝 :: Font

---
+++ Togather/Togather/Resources/Font/Font.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+extension Font {
+    
+    //MARK: - typography
+    
+    static let typography = custom("Pretendard-Bold", size: 40)
+    
+    //MARK: - title
+
+    static let titleXLm = custom("Pretendard-Medium", size: 32)
+    static let titleXLb = custom("Pretendard-Bold", size: 32)
+    static let title1m = custom("Pretendard-Medium", size: 28)
+    static let title1b = custom("Pretendard-Bold", size: 28)
+    static let title2m = custom("Pretendard-Medium", size: 24)
+    static let title2b = custom("Pretendard-Bold", size: 24)
+    static let title3m = custom("Pretendard-Medium", size: 20)
+    static let title3b = custom("Pretendard-Bold", size: 20)
+    
+    //MARK: - mainText
+
+    static let mainText1m = custom("Pretendard-Medium", size: 18)
+    static let maintext1b = custom("Pretendard-Bold", size: 18)
+    static let maintext2m = custom("Pretendard-Medium", size: 16)
+    static let maintext2b = custom("Pretendard-Bold", size: 16)
+    
+    //MARK: - indicator
+
+    static let indicator = custom("Pretendard-Bold", size: 14)
+}

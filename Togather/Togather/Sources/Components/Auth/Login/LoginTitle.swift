190d5d8268226cba2dcb38ef42d4c4f044657803
BJCHO0501 <bjcho1503@naver.com>
2022-10-17 14:53:20 +0900

    📝 :: LoginTitle

---
+++ Togather/Togather/Sources/Components/Auth/Login/LoginTitle.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct LoginTitle: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("마음 맞는 팀원을 찾는 \n간단한 방법.")
+                .foregroundColor(.text)
+                .font(.titleXLb)
+            Text("ToGather")
+                .foregroundColor(.main)
+                .font(.titleXLb)
+        }
+    }
+}
+
+struct LoginTitle_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginTitle()
+    }
+}

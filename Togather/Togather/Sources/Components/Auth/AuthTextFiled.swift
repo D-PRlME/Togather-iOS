b1d04bad60ec2c94dc5251a9837d5ce0c3138275
BJCHO0501 <bjcho1503@naver.com>
2022-10-18 11:03:59 +0900

    📝 :: AuthTextFiled

--- Togather/Togather/Sources/Components/Auth/AuthTextFiled.swift
+++ Togather/Togather/Sources/Components/Auth/AuthTextFiled.swift
@@ -5,28 +5,34 @@ struct AuthTextFiled: View {
     let isSecret: Bool
     @State private var isShow = true
     @Binding var text: String
+    @Binding var message: String
+    @Binding var isError: Bool
     
-    init (
+    init(
         placeholder: String,
-        isSecret: Bool,
-        text: Binding<String>
+        isSecret: Bool = false,
+        text: Binding<String>,
+        message: Binding<String> = .constant(""),
+        isError: Binding<Bool> = .constant(false)
     ) {
         self.placeholder = placeholder
         self.isSecret = isSecret
         self._text = text
+        self._message = message
+        self._isError = isError
     }
     
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             ZStack(alignment: .trailing) {
                 HStack {
-                    if isShow {
+                    if isSecret ? !isShow : true {
                         TextField(
                             placeholder,
                             text: $text
                         )
-                        .disableAutocorrection(true)
                         .autocapitalization(.none)
+                        .disableAutocorrection(true)
                         .font(.title3m)
                         .padding(.leading, 10)
                     } else {
@@ -34,34 +40,41 @@ struct AuthTextFiled: View {
                             placeholder,
                             text: $text
                         )
-                        .disableAutocorrection(true)
                         .autocapitalization(.none)
+                        .disableAutocorrection(true)
                         .font(.title3m)
                         .padding(.leading, 10)
                     }
                 }
                 .frame(height: 48)
-                .background(Color("white-Elevated1"))
+                .background(Color.whiteElevated1)
                 .cornerRadius(8)
                 .overlay(
                     RoundedRectangle(cornerRadius: 8)
                         .stroke(style: .init(lineWidth: 1))
-                        .foregroundColor(Color("white-Elevated3"))
+                        .foregroundColor(isError ? .error : .whiteElevated3)
                 )
                 
                 if isSecret {
                     Button {
                         self.isShow.toggle()
                     } label: {
-                        Image(systemName: isShow ? "eye.slash" : "eye")
-                     }
+                        Image(isShow ? "EyeDisable" : "Eye")
+                    }
                     .frame(width: 28, height: 28)
-                    .foregroundColor(Color("white-Elevated3"))
                     .padding(.trailing, 10)
                 }
             }
-            
-            
+            if !message.isEmpty {
+                HStack {
+                    Text(message)
+                        .font(.indicator)
+                        .foregroundColor(isError ? Color.error : Color.whiteElevated4)
+                        .padding(.top, 8)
+                        .padding(.leading, 12)
+                    Spacer()
+                }
+            }
         }
     }
 }
@@ -71,7 +84,9 @@ struct AuthTextFiled_Previews: PreviewProvider {
         AuthTextFiled(
             placeholder: "아이디",
             isSecret: true,
-            text: .constant("")
+            text: .constant("hi"),
+            message: .constant("dsm.hs.kr 도메인을 사용하는 이메일을 사용하세요"),
+            isError: .constant(false)
         )
     }
 }

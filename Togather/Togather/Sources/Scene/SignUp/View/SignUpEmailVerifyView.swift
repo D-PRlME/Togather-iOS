5e013eef6d2e3ec5aa7aea19d52592234df974f6
BJCHO0501 <bjcho1503@naver.com>
2022-10-24 23:28:06 +0900

    📝 :: SignUpEmailVerifyView

---
+++ Togather/Togather/Sources/Scene/SignUp/View/SignUpEmailVerifyView.swift
@@ -0,0 +1,82 @@
+import SwiftUI
+
+struct EmailVerify: View {
+    
+    @Binding var email: String
+    @Binding var password: String
+    @Binding var name: String
+    
+    @StateObject var SignUpVM = SignUpViewModel()
+    
+    private func ButtonAtivation() -> Bool {
+        if SignUpVM.code.count == 6 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    var body: some View {
+        GeometryReader { proxy in
+            ZStack {
+                NavigationLink(
+                    destination:
+                        signUpSuccess(
+                            email: $email,
+                            password: $password,
+                            name: $name
+                        ),
+                    tag: 2,
+                    selection: $SignUpVM.isSuccess
+                ) { EmptyView() }
+                
+                
+                VStack(alignment: .center, spacing: 12) {
+                    Spacer()
+                    EmailVerifyTitle(
+                        email: email
+                    )
+                    
+                    EmailVerifyTextField(
+                        code: $SignUpVM.code
+                    )
+                    
+                    VStack(spacing: 0) {
+                        Rectangle()
+                            .frame(height: 50)
+                            .opacity(0)
+                        Image("PostBox")
+                            .resizable()
+                            .frame(width: 150, height: 150)
+                    }
+                    Spacer()
+                        
+                    AuthButton(
+                        text: "다음",
+                        action: {
+                            SignUpVM.emailVerify()
+                        }
+                    )
+                } //Vstack
+                .padding(.horizontal, 16)
+                .padding(.bottom, 16)
+                .onAppear {
+                    SignUpVM.email = email
+                    SignUpVM.sendEmailToUser()
+                }
+                BackBtn()
+            }
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+struct EmailVerify_Previews: PreviewProvider {
+    static var previews: some View {
+        EmailVerify(
+            email: .constant("bj"),
+            password: .constant("dd"),
+            name: .constant("조병진")
+        )
+    }
+}

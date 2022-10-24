2ddd99264b2cdc6ded1c092f3a148835c8887d29
BJCHO0501 <bjcho1503@naver.com>
2022-10-24 23:28:16 +0900

    📝 :: SignUpTextField

---
+++ Togather/Togather/Sources/Scene/SignUp/SignUpTextField/SignUpTextField.swift
@@ -0,0 +1,52 @@
+//
+//  SignUpTextField.swift
+//  Togather
+//
+//  Created by 조병진 on 2022/10/18.
+//
+
+import SwiftUI
+
+struct SignUpTextField: View {
+    @Binding var email: String
+    @Binding var password: String
+    @Binding var name: String
+    @Binding var emailError: Bool
+    @Binding var passwordError: Bool
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            AuthTextField(
+                placeholder: "이메일",
+                text: $email,
+                message: "dsm.hs.kr 도메인을 사용하는 이메일을 사용하세요",
+                isError: $emailError
+            )
+            
+            AuthTextField(
+                placeholder: "비밀번호",
+                isSecret: true,
+                text: $password,
+                message: "8자리 이상, 숫자, 영어 소문자, 특수문자를 포함해야 합니다 ",
+                isError: $passwordError
+            )
+            
+            AuthTextField(
+                placeholder: "이름",
+                text: $name
+            )
+        }
+    }
+}
+
+struct SignUpTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpTextField(
+            email: .constant(""),
+            password: .constant(""),
+            name: .constant(""),
+            emailError: .constant(false),
+            passwordError: .constant(true)
+        )
+    }
+}

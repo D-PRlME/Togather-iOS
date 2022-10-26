a60e2e64cac609fde7686456109fe3212dec3bd5
hsj._.06 <gtw030488@gmail.com>
2022-10-26 17:00:19 +0900

    🐛 :: 에러 수정

--- Togather/Togather/Sources/Application/ContentView.swift
+++ Togather/Togather/Sources/Application/ContentView.swift
@@ -9,7 +9,6 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             LoginView()
-//            HomeView()
                 .navigationBarHidden(true)
         }
     }

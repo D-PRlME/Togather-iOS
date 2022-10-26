41fb58b4bd89c4ff12de8f931896624abe0112ef
BJCHO0501 <bjcho1503@naver.com>
2022-10-26 22:22:34 +0900

    📝 :: WritingButton

---
+++ Togather/Togather/Sources/Components/WritingPost/WritingButton.swift
@@ -0,0 +1,55 @@
+//
+//  WritingButton.swift
+//  Togather
+//
+//  Created by 조병진 on 2022/10/26.
+//
+
+import SwiftUI
+
+struct WritingButton: View {
+    var title: String
+    var backgroundColor: Color
+    var cornerColor: Color
+    var action: () -> Void
+    
+    init(
+        title: String,
+        backgroundColor: Color,
+        cornerColor: Color,
+        action: @escaping () -> Void
+    ) {
+        self.title = title
+        self.backgroundColor = backgroundColor
+        self.cornerColor = cornerColor
+        self.action = action
+    }
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.maintext1b)
+                .foregroundColor(.text)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 8)
+                .background(backgroundColor)
+                .cornerRadius(37)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 37)
+                        .stroke(lineWidth: 2)
+                        .foregroundColor(cornerColor)
+                )
+        }
+    }
+}
+
+struct WritingButton_Previews: PreviewProvider {
+    static var previews: some View {
+        WritingButton(
+            title: "글쓰기",
+            backgroundColor: .main,
+            cornerColor: .mainDarken,
+            action: { }
+        )
+    }
+}

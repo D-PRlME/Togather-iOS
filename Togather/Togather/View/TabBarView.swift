ae47e124ef98f084b9ecb40742ac492185102030
hsj._.06 <gtw030488@gmail.com>
2022-10-28 21:11:09 +0900

    📝 :: TabBarView

--- Togather/Togather/View/TabBarView.swift
+++ Togather/Togather/View/TabBarView.swift
@@ -8,14 +8,22 @@ enum TabIndex {
 struct TabBarView: View {
     @State var tabIndex = TabIndex.home
     @State var showModal = false
-    
     var body: some View {
         GeometryReader { proxy in
             VStack(spacing: 0) {
                 Spacer()
                 ZStack(alignment: .bottom) {
-                    ShowView(tabIndex: tabIndex)
-                        .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.bottom == 0 ? 0 : 40)
+                    TabView(selection: $tabIndex) {
+                        HomeView()
+                            .tag(TabIndex.home)
+                        ChatListView()
+                            .tag(TabIndex.chat)
+                        SearchView()
+                            .tag(TabIndex.search)
+                        MyView()
+                            .tag(TabIndex.mypage)
+                    }
+                    .tabViewStyle(.page(indexDisplayMode: .never))
                     ZStack {
                         VStack(spacing: 0) {
                             Rectangle()
@@ -26,48 +34,45 @@ struct TabBarView: View {
                                 .frame(width: proxy.size.width, height: UIApplication.shared.windows.first?.safeAreaInsets.bottom == 0 ? 86 : 106)
                         }
                         HStack(alignment: .top, spacing: 0) {
-                            VStack {
-                                Image(tabIndex == .home ? "HomeOn" : "HomeOff")
-                                    .font(.system(size: 25))
-                                    .frame(width: 28, height: 28)
-                                Text("홈")
-                                    .font(.custom("Pretendard-Bold", size: 14))
-                                    .foregroundColor(tabIndex == .home ? Color("TabBarSelectedColor") : Color("TabBarNoSelectedColor"))
-                                Spacer()
-                            }
-                            .frame(width: proxy.size.width/6, height: 50)
-                            .onTapGesture {
-                                print("----------------------------------")
-                                print("Home View")
-                                print("----------------------------------")
-                                tabIndex = .home
-                            }
-                            VStack {
-                                Image(tabIndex == .chat ? "ChatOn" : "ChatOff")
-                                    .font(.system(size: 25))
-                                    .frame(width: 28, height: 28)
-                                Text("채팅")
-                                    .font(.custom("Pretendard-Bold", size: 14))
-                                    .foregroundColor(tabIndex == .chat ? Color("TabBarSelectedColor") : Color("TabBarNoSelectedColor"))
-                                Spacer()
-                            }
-                            .frame(width: proxy.size.width/6, height: 50)
-                            .onTapGesture {
-                                print("----------------------------------")
-                                print("ChatList View")
-                                print("----------------------------------")
-                                tabIndex = .chat
-                            }
                             Button {
-                                print("----------------------------------")
-                                print("Writing View")
-                                print("----------------------------------")
-                                print("MyView -> 계정 정보 수정 -> EditInfo")
-                                showModal.toggle()
+                                tabIndex = .home
+                            } label: {
+                                VStack {
+                                    Image(tabIndex == .home ? "HomeOn" : "HomeOff")
+                                        .font(.system(size: 25))
+                                        .foregroundColor(tabIndex == .home ? Color("TabBarSelectedColor") : Color("TabBarNoSelectedColor"))
+                                        .frame(width: 28, height: 28)
+                                    Text("홈")
+                                        .font(.custom("Pretendard-Bold", size: 14))
+                                        .foregroundColor(tabIndex == .home ? Color("TabBarSelectedColor") : Color("TabBarNoSelectedColor"))
+                                    Spacer()
+                                }
+                                .frame(width: proxy.size.width/6, height: 50)
+                            }
+                            .buttonStyle(.plain)
+                            Button {
+                                tabIndex = .chat
+                            } label: {
+                                VStack {
+                                    Image(tabIndex == .chat ? "ChatOn" : "ChatOff")
+                                        .font(.system(size: 25))
+                                        .foregroundColor(tabIndex == .chat ? Color("TabBarSelectedColor") : Color("TabBarNoSelectedColor"))
+                                        .frame(width: 28, height: 28)
+                                    Text("채팅")
+                                        .font(.custom("Pretendard-Bold", size: 14))
+                                        .foregroundColor(tabIndex == .chat ? Color("TabBarSelectedColor") : Color("TabBarNoSelectedColor"))
+                                    Spacer()
+                                }
+                                .frame(width: proxy.size.width/6, height: 50)
+                            }
+                            .buttonStyle(.plain)
+                            Button {
+                                self.showModal.toggle()
                             } label: {
                                 VStack {
                                     Image(tabIndex == .write ? "WritingOn" : "WritingOff")
                                         .font(.system(size: 25))
+                                        .foregroundColor(tabIndex == .write ? Color("TabBarSelectedColor") : Color("TabBarNoSelectedColor"))
                                         .frame(width: 28, height: 28)
                                     Text("글쓰기")
                                         .font(.custom("Pretendard-Bold", size: 14))
@@ -75,43 +80,44 @@ struct TabBarView: View {
                                     Spacer()
                                 }
                                 .frame(width: proxy.size.width/6, height: 50)
-                            } .sheet(isPresented: self.$showModal) {
+                            }
+                            .buttonStyle(.plain)
+                            .sheet(isPresented: self.$showModal) {
                                 WritingView(showModal: self.$showModal)
                             }
                             .buttonStyle(.plain)
-                            VStack {
-                                Image(tabIndex == .search ? "SearchOn" : "SearchOff")
-                                    .font(.system(size: 25))
-                                    .foregroundColor(tabIndex == .search ? Color("TabBarSelectedColor") : Color("TabBarNoSelectedColor"))
-                                    .frame(width: 28, height: 28)
-                                Text("검색")
-                                    .font(.custom("Pretendard-Bold", size: 14))
-                                    .foregroundColor(tabIndex == .search ? Color("TabBarSelectedColor") : Color("TabBarNoSelectedColor"))
-                                Spacer()
-                            }
-                            .frame(width: proxy.size.width/6, height: 50)
-                            .onTapGesture {
-                                print("----------------------------------")
-                                print("Search View")
-                                print("----------------------------------")
+                            Button {
                                 tabIndex = .search
+                            } label: {
+                                VStack {
+                                    Image(tabIndex == .search ? "SearchOn" : "SearchOff")
+                                        .font(.system(size: 25))
+                                        .foregroundColor(tabIndex == .search ? Color("TabBarSelectedColor") : Color("TabBarNoSelectedColor"))
+                                        .frame(width: 28, height: 28)
+                                    Text("검색")
+                                        .font(.custom("Pretendard-Bold", size: 14))
+                                        .foregroundColor(tabIndex == .search ? Color("TabBarSelectedColor") : Color("TabBarNoSelectedColor"))
+                                    Spacer()
+                                }
+                                .frame(width: proxy.size.width/6, height: 50)
                             }
-                            VStack {
-                                Image(tabIndex == .mypage ? "MyOn" : "MyOff")
-                                    .font(.system(size: 25))
-                                    .frame(width: 28, height: 28)
-                                Text("나")
-                                    .font(.custom("Pretendard-Bold", size: 14))
-                                    .foregroundColor(tabIndex == .mypage ? Color("TabBarSelectedColor") : Color("TabBarNoSelectedColor"))
-                                Spacer()
-                            }
-                            .frame(width: proxy.size.width/6, height: 50)
-                            .onTapGesture {
-                                print("—————————————————")
-                                print("My View")
-                                print("—————————————————")
+                            .buttonStyle(.plain)
+                            Button {
                                 tabIndex = .mypage
+                            } label: {
+                                VStack {
+                                    Image(tabIndex == .mypage ? "MyOn" : "MyOff")
+                                        .font(.system(size: 25))
+                                        .foregroundColor(tabIndex == .mypage ? Color("TabBarSelectedColor") : Color("TabBarNoSelectedColor"))
+                                        .frame(width: 28, height: 28)
+                                    Text("나")
+                                        .font(.custom("Pretendard-Bold", size: 14))
+                                        .foregroundColor(tabIndex == .mypage ? Color("TabBarSelectedColor") : Color("TabBarNoSelectedColor"))
+                                    Spacer()
+                                }
+                                .frame(width: proxy.size.width/6, height: 50)
                             }
+                            .buttonStyle(.plain)
                         }
                         .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom == 0 ? 5 : 25)
                     }

87d45bdfedc2936ae1872b605b8f301f0354af02
BJCHO0501 <bjcho1503@naver.com>
2022-10-29 18:23:56 +0900

    📝 :: SearchTextField

---
+++ Togather/Togather/Sources/Components/Search/SearchTextField.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+
+struct SearchTextField: View {
+    var placeholder: String
+    @Binding var text: String
+    var action: () -> Void
+    
+    var body: some View {
+        HStack {
+            TextField(placeholder, text: $text)
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
+                .font(.title3m)
+                .multilineTextAlignment(.leading)
+            
+            Image("SearchOff")
+                .resizable()
+                .frame(width: 28, height: 28)
+                .onTapGesture {
+                    action()
+                }
+        }
+        .padding(12)
+        .background(Color.whiteElevated1)
+        .cornerRadius(6)
+        .overlay(
+            RoundedRectangle(cornerRadius: 6)
+                .stroke(lineWidth: 1)
+                .foregroundColor(.whiteElevated3)
+        )
+    }
+}
+
+struct SearchTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchTextField(
+            placeholder: "검색",
+            text: .constant(""),
+            action: { }
+        )
+    }
+}

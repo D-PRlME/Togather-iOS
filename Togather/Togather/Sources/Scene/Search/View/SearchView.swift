fbfb76091133ce1c4bebbf06eb9efc3b5f6868b9
BJCHO0501 <bjcho1503@naver.com>
2022-10-29 21:09:28 +0900

    🐛 :: SearchView

--- Togather/Togather/Sources/Scene/Search/View/SearchView.swift
+++ Togather/Togather/Sources/Scene/Search/View/SearchView.swift
@@ -11,6 +11,8 @@ struct SearchView: View {
     @State var stringTagList: [String] = []
     @State var goTagList: Bool = false
     
+    @State var isClose: Bool = false
+    
     @StateObject var searchViewModel = SearchViewModel()
     
     var body: some View {
@@ -59,98 +61,30 @@ struct SearchView: View {
                         }
                     )
                     
-                    ScrollView {
+                    ScrollView(showsIndicators: false) {
                         ForEach(searchViewModel.postList, id: \.post_id) { data in
-                            Button {
-                                GoPostDetail = true
-                                print("homeview :",data.post_id)
-                            } label: {
-                                VStack(alignment: .leading, spacing: 8) {
-                                    Text(data.title)
-                                        .foregroundColor(.text)
-                                        .font(.title2b)
-                                        .multilineTextAlignment(.leading)
-                                        .padding(.leading, 5)
-                                    
-                                    FlowLayout(mode: .scrollable,
-                                               items: data.tags,
-                                               itemSpacing: 3) {
-                                        Text($0.name)
-                                            .foregroundColor(.text)
-                                            .font(.maintext2m)
-                                            .padding(.horizontal, 12)
-                                            .padding(.vertical, 6)
-                                            .background(Color.whiteElevated2)
-                                            .cornerRadius(37)
-                                            .overlay(
-                                                RoundedRectangle(cornerRadius: 37)
-                                                    .stroke(style: .init(lineWidth: 1))
-                                                    .foregroundColor(.whiteElevated3)
-                                            )
-                                    }
-                                    
-                                    Divider()
-                                    
-                                    HStack(spacing: 8) {
-                                        KFImage.url(URL(string: data.users.profile_image_url))
-                                            .placeholder {
-                                                Circle().fill(Color.whiteElevated2)
-                                            }
-                                            .resizable()
-                                            .scaledToFill()
-                                            .clipShape(Circle())
-                                            .frame(width: 33, height: 33)
-                                            .padding(.horizontal, 4)
-                                            .overlay(Circle().stroke(lineWidth: 1).foregroundColor(.whiteElevated3))
-                                        
-                                        Text(data.users.user_name)
-                                            .foregroundColor(.black)
-                                            .font(.maintext2m)
-                                        
-                                        Spacer()
-                                        
-                                        Image("GrayHeart")
-                                            .resizable()
-                                            .frame(width: 16, height: 13)
-                                        
-                                        Text(String(data.like_count))
-                                            .foregroundColor(.whiteElevated4)
-                                            .font(.maintext2m)
-                                        Image("SeperateBar")
-                                            .resizable()
-                                            .frame(width: 1, height: 15)
-                                        Text(data.created_at)
-                                            .foregroundColor(.whiteElevated4)
-                                            .font(.maintext2m)
-                                    }
-                                }
-                                .padding(12)
-                                .background(Color.whiteElevated1)
-                                .cornerRadius(8)
-                                .padding(.vertical, 6)
-                            }
-                            .sheet(
-                                isPresented: $GoPostDetail,
-                                onDismiss: {
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                                        if !searchViewModel.title.isEmpty {
-                                            searchViewModel.tag.removeAll()
-                                            searchViewModel.postTitle()
-                                        } else if tagBtnValue != "" {
-                                            searchViewModel.postTag()
-                                        }
-                                    }
-                                },
-                                content: {
-                                    PostDetail(
-                                        showModal: self.$GoPostDetail,
-                                        postID: data.post_id
-                                    )
-                                }
+                            PostForm(
+                                close: $isClose,
+                                postData: data
                             )
+                        }
+                        .onChange(of: isClose) { newValue in
+                            if newValue {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                                    if !searchViewModel.title.isEmpty {
+                                        searchViewModel.tag.removeAll()
+                                        searchViewModel.postTitle()
+                                    } else if tagBtnValue != "" {
+                                        searchViewModel.tag = tagBtnValue
+                                        searchViewModel.postTag()
+                                    }
+                                    self.isClose = false
+                                }
+                            }
                         }
                     }
                     .padding(.horizontal, 16)
+                    .padding(.bottom, proxy.safeAreaInsets.bottom == 0 ? 106 : 86)
                 }
             }
             Spacer()

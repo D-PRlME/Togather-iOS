160f726409a761f585d4258d15156293aea65471
BJCHO0501 <bjcho1503@naver.com>
2022-10-29 14:09:00 +0900

    📝 :: SearchTagListView

--- Togather/Togather/Sources/Scene/TagList/View/SearchTagListView.swift
+++ Togather/Togather/Sources/Scene/TagList/View/SearchTagListView.swift
@@ -3,13 +3,9 @@ import Kingfisher
 
 struct SearchTagListView: View {
     @StateObject var tagVM = TagViewModel()
-    //    @State var search: String = ""
     
     @Binding var goBack: Bool
-    
     @Binding var tagBtnValue: String
-    
-    @StateObject var searchVM = SearchViewModel()
     
     var body: some View {
         VStack {
@@ -19,8 +15,7 @@ struct SearchTagListView: View {
                 Image("CloseBtn")
                     .resizable()
                     .frame(width: 30, height: 30)
-                    .padding(.trailing, 16)
-                    .padding(.top, 16)
+                    .padding([.trailing, .top], 16)
                     .onTapGesture {
                         self.goBack = false
                     }
@@ -28,46 +23,20 @@ struct SearchTagListView: View {
             
             HStack {
                 Text("모든 태그 보기")
-                    .font(.custom("Pretendard-Bold", size: 28))
-                    .foregroundColor(.black)
+                    .font(.title1b)
+                    .foregroundColor(.text)
                     .padding(.horizontal, 16)
                 
                 Spacer()
             }
             
-            
-            //            HStack {
-            //                TextField("검색", text: $search)
-            //                    .autocapitalization(.none)
-            //                    .disableAutocorrection(true)
-            //                    .font(.custom("Pretendard-Medium", size: 20))
-            //                    .foregroundColor(.black)
-            //                Image("SearchOff")
-            //                    .resizable()
-            //                    .frame(width: 28, height: 28)
-            //            }
-            //            .padding(12)
-            //            .background(Color(red: 0.97, green: 0.97, blue: 0.97))
-            //            .cornerRadius(6)
-            //            .padding(1)
-            //            .background(Color("TabBarStroke"))
-            //            .cornerRadius(6)
-            //            .padding(.horizontal, 16)
-            
             //list
             List(tagVM.tagValues, id: \.self) { data in
                 Button {
-//                    tagLists.removeAll()
-//                    if tagLists.contains(data.name){
-//                    } else {
-//                        tagLists.append(data.name)
-//                    }
                     if tagBtnValue == data.name {
                         tagBtnValue = ""
-                        print("\(data.name) 태그 끄기")
                     } else {
                         tagBtnValue = data.name
-                        print("\(data.name) 태그 켜기")
                     }
                 } label: {
                     HStack {
@@ -78,15 +47,14 @@ struct SearchTagListView: View {
                             Rectangle()
                                 .frame(width: 45, height: 45)
                                 .cornerRadius(15)
-                                .foregroundColor(.secondary)
-                                .opacity(0.1)
+                                .foregroundColor(.whiteElevated3)
                         }
                         .resizable()
                         .frame(width: 45, height: 45)
                         .cornerRadius(15)
                         .overlay(
                             Rectangle()
-                                .foregroundColor(.black)
+                                .foregroundColor(.text)
                                 .frame(width: 45, height: 45)
                                 .opacity(0.5)
                                 .cornerRadius(15)
@@ -99,7 +67,8 @@ struct SearchTagListView: View {
                         )
                         
                         Text("\(data.name)")
-                            .font(.custom("Pretendard-Medium", size: 20))
+                            .font(.title3m)
+                            .foregroundColor(.text)
                     }
                 }
             }
@@ -108,13 +77,14 @@ struct SearchTagListView: View {
         .onAppear {
             tagVM.GstTagDatas()
         }
-        .onDisappear() {
-        }
     }
 }
 
-//struct TagListView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        TagListView()
-//    }
-//}
+struct SearchTagListView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchTagListView(
+            goBack: .constant(true),
+            tagBtnValue: .constant("Node.js")
+        )
+    }
+}

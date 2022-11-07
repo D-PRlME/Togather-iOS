4d567e8ff9b19d6a178b85e311c7a4d63b90820f
BJCHO0501 <bjcho1503@naver.com>
2022-11-07 18:55:24 +0900

    📝 :: EditInfoView

--- Togather/Togather/Sources/Scene/MyView/View/myview ->/EditInfoView.swift
+++ Togather/Togather/Sources/Scene/MyView/View/myview ->/EditInfoView.swift
@@ -34,14 +34,13 @@ struct EditInfo: View {
                         HStack(spacing: 12) {
                             KFImage.url(URL(string: changeMyInfoVM.profileImageLink))
                                 .placeholder {
-                                    Circle().fill(Color.secondary)
+                                    Circle().fill(Color.whiteElevated3)
                                         .frame(width: 48, height: 48)
                                 }
                                 .resizable()
                                 .clipShape(Circle())
                                 .frame(width: 48, height: 48)
-                                .overlay(Circle().stroke().foregroundColor(Color("TabBarStroke")))
-                                .padding(.leading, 10)
+                                .overlay(Circle().stroke().foregroundColor(.whiteElevated3))
                             Button  {
                                 self.showImagePicker.toggle()
                             } label: {
@@ -59,27 +58,20 @@ struct EditInfo: View {
                             .sheet(isPresented: $showImagePicker) {
                                 ImagePicker(image: $changeMyInfoVM.image)
                             }
-                            .onChange(of: changeMyInfoVM.image ?? UIImage()) { newValue in
-                                changeMyInfoVM.image = newValue
-                                changeMyInfoVM.updateProfileImage()
+                            .onChange(of: selectedImage) { newValue in
+//                                changeMyInfoVM.
                             }
                             Spacer()
                         }
                         .padding(.top, 26)
                         
+                        
                         //ID
-                        TextField("이름", text: $changeMyInfoVM.name)
-                            .autocapitalization(.none)
-                            .disableAutocorrection(true)
-                            .font(.custom("Pretendard-Medium", size: 20))
-                            .foregroundColor(.black)
-                            .padding(12)
-                            .background(Color(red: 0.97, green: 0.97, blue: 0.97))
-                            .cornerRadius(6)
-                            .padding(1)
-                            .background(Color("TabBarStroke"))
-                            .cornerRadius(6)
-                            .padding(.top, 12)
+                        EditInfoTextField(
+                            placeholder: "이름",
+                            text: $changeMyInfoVM.name
+                        )
+                        .padding(.top, 12)
                         
                         //Email
                         VStack(alignment: .leading ,spacing: 0) {
@@ -87,15 +79,15 @@ struct EditInfo: View {
                                 .frame(height: 0)
                             
                             Text(Account.email ?? "")
-                                .font(.custom("Pretendard-Medium", size: 20))
-                                .foregroundColor(.secondary)
+                                .font(.title3m)
+                                .foregroundColor(.whiteElevated4)
                                 
                         }
                         .padding(12)
                         .background(Color(red: 0.97, green: 0.97, blue: 0.97))
                         .cornerRadius(6)
                         .padding(1)
-                        .background(Color("TabBarStroke"))
+                        .background(Color.whiteElevated3)
                         .cornerRadius(6)
                         .padding(.top, 8)
                     
@@ -103,6 +95,7 @@ struct EditInfo: View {
                         //Introduce
                         ZStack(alignment: .topLeading) {
                             TextEditor(text: $changeMyInfoVM.introduce)
+//                                .scrollContentBackground(.hidden)
                                 .font(.custom("Pretendard-Medium", size: 20))
                                 .foregroundColor(.black)
                                 .padding(EdgeInsets(top: 12, leading: 10, bottom: 30, trailing: 10))
@@ -148,8 +141,8 @@ struct EditInfo: View {
                         VStack {
                             HStack {
                                 Text("내 포지션")
-                                    .font(.custom("Pretendard-ExtraBold", size: 14))
-                                    .foregroundColor(.black.opacity(0.5))
+                                    .font(.indicator)
+                                    .foregroundColor(.whiteElevated4)
                                 
                                 Spacer()
                             }
@@ -160,7 +153,7 @@ struct EditInfo: View {
                                 
                                 Text(index)
                                     .foregroundColor(.black)
-                                    .font(.custom("Pretendard-Medium", size: 16))
+                                    .font(.maintext2m)
                                     .padding(.horizontal, 12)
                                     .padding(.vertical, 6)
                                     .background(changeMyInfoVM.positions.contains(index.PositionTypeChange) ? Color(red: 0.925, green: 0.792, blue: 0.357) : Color(red: 0.905, green: 0.905, blue: 0.905))
@@ -180,37 +173,31 @@ struct EditInfo: View {
                         
                         Spacer()
                         HStack {
-                            Button {
-                                if (changeMyInfoVM.introduce.count <= introduceTextLimit) {
-                                    changeMyInfoVM.changeMyInfo()
-                                } else {
-                                    alertMessage = "소개 글자수는 100자 이하입니다."
-                                    showAlert = true
+                            PostButton(
+                                title: "저장",
+                                backgroundColor: .main,
+                                cornerColor: .mainDarken,
+                                action: {
+                                    if (changeMyInfoVM.introduce.count <= introduceTextLimit) {
+                                        changeMyInfoVM.changeMyInfo()
+                                    } else {
+                                        alertMessage = "소개 글자수는 100자 이하입니다."
+                                        showAlert = true
+                                    }
                                 }
-                            } label: {
-                                Text("저장")
-                                    .foregroundColor(.black)
-                                    .font(.custom("Pretendard-Bold", size: 18))
-                                    .padding(.horizontal, 16)
-                                    .padding(.vertical, 8)
-                                    .background(Color(red: 0.882, green: 0.678, blue: 0.004))
-                                    .cornerRadius(37)
-                                    .padding(2)
-                                    .background(Color("YellowStroke"))
-                                    .cornerRadius(37)
-                            }
+                            )
                             
                             Spacer()
                             NavigationLink(destination: DeleteAccount(goSignView: $goBack)) {
                                 Text("계정 삭제")
                                     .foregroundColor(.black)
-                                    .font(.custom("Pretendard-Bold", size: 18))
+                                    .font(.maintext1b)
                                     .padding(.horizontal, 16)
                                     .padding(.vertical, 8)
-                                    .background(Color(red: 0.995, green: 0.238, blue: 0.238))
+                                    .background(Color.error)
                                     .cornerRadius(37)
                                     .padding(2)
-                                    .background(Color("RedStroke"))
+                                    .background(Color.redDarken)
                                     .cornerRadius(37)
                             }
                             .buttonStyle(.plain)

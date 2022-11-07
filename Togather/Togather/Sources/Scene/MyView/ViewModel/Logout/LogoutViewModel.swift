7cf654b5a79c68c8168ef5e4287ca12178126303
BJCHO0501 <bjcho1503@naver.com>
2022-11-07 18:55:07 +0900

    📝 :: LogoutViewModel

---
+++ Togather/Togather/Sources/Scene/MyView/ViewModel/Logout/LogoutViewModel.swift
@@ -0,0 +1,27 @@
+import Foundation
+import Moya
+
+class LogoutViewModel: ObservableObject {
+    let userClient = MoyaProvider<UserService>(plugins: [MoyaLoggerPlugin()])
+    
+    @Published var isSucced: Bool = false
+    
+    func logout() {
+        userClient.request(.logout) { res in
+            switch res {
+            case .success(let result):
+                switch result.statusCode {
+                case 204:
+                    Account.deleteAccount()
+                    Token.removeToken()
+                    self.isSucced = true
+                    print("로그아웃")
+                default:
+                    print("\(result.statusCode)")
+                }
+            case .failure(let err):
+                print("⛔️logout error: \(err.localizedDescription)")
+            }
+        }
+    }
+}

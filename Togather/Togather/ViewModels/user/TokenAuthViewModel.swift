4f3f1ff04b5a976de88ed57fec9cae60f646424f
BJCHO0501 <bjcho1503@naver.com>
2022-11-07 18:53:44 +0900

    📝 :: TokenAuthViewModel

---
+++ Togather/Togather/ViewModels/user/TokenAuthViewModel.swift
@@ -0,0 +1,35 @@
+import Foundation
+import Moya
+
+class TokenAuthViewModel: ObservableObject {
+    let UserClient = MoyaProvider<UserService>(plugins: [MoyaLoggerPlugin()])
+    
+    func tokenReissueClient() {
+        UserClient.request(.tokenReissue) { res in
+            switch res {
+            case .success(let result):
+                switch result.statusCode {
+                case 200:
+                    DispatchQueue.main.async {
+                        let decoder = JSONDecoder()
+                        if let data = try? decoder.decode(TokenModel.self, from: result.data) {
+                            Token.accessToken = data.access_token
+                            Token.refreshToken = data.refresh_token
+                            print("token 재발급")
+                            print("🔊\(data.expired_at)")
+                        } else {
+                            print("⚠️tokenAuth docoder error")
+                        }
+                    }
+                case 404:
+                    print("알수없는 토큰")
+                default:
+                    print("\(result.statusCode)")
+                }
+                
+            case .failure(let err):
+                print("⛔️tokenAuth Error: \(err.localizedDescription)")
+            }
+        }
+    }
+}

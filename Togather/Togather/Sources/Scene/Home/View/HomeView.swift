44d73ff2d49f7954e29a4191a37e4482b0f75098
hsj._.06 <gtw030488@gmail.com>
2022-11-09 21:44:51 +0900

    📝 :: lint rules

--- Togather/Togather/Sources/Scene/Home/View/HomeView.swift
+++ Togather/Togather/Sources/Scene/Home/View/HomeView.swift
@@ -8,53 +8,51 @@ import SwiftUIPullToRefresh
 struct HomeView: View {
     
     @State private var isClose: Bool = false
-
+    
     @StateObject var homeViewModel = HomeViewModel()
     let animation = Animation
         .linear
         .repeatForever(autoreverses: false)
     
     var body: some View {
-        GeometryReader { proxy in
-            ZStack() {
-                ColorManager.BackgroundColor.ignoresSafeArea()
-                VStack(spacing: 0) {
-                    RefreshableScrollView(onRefresh: { done in
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                            homeViewModel.post()
-                            homeViewModel.GetTagList()
-                            done()
-                        }
-                    }) {
-                        VStack(spacing: 0) {
-                            TagSelectList(
-                                tags: $homeViewModel.tagList
+        ZStack {
+            ColorManager.BackgroundColor.ignoresSafeArea()
+            VStack(spacing: 0) {
+                RefreshableScrollView { done in
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        homeViewModel.post()
+                        homeViewModel.getTagList()
+                        done()
+                    }
+                } content: {
+                    VStack(spacing: 0) {
+                        TagSelectList(
+                            tags: $homeViewModel.tagList
+                        )
+                        
+                        ForEach(homeViewModel.postList, id: \.postID) { data in
+                            PostForm(
+                                close: $isClose,
+                                postData: data
                             )
-                            
-                            ForEach(homeViewModel.postList, id: \.post_id) { data in
-                                PostForm(
-                                    close: $isClose,
-                                    postData: data
-                                )
-                                .padding(.horizontal, 14)
-                            }
+                            .padding(.horizontal, 14)
                         }
-                        .padding(.top, 10)
                     }
-                    .padding(.top, 12)
-                    .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom == 0 ? 86 : 106)
-                    .onAppear() {
-                        DispatchQueue.global().async {
-                            homeViewModel.GetTagList()
+                    .padding(.top, 10)
+                }
+                .padding(.top, 12)
+                .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom == 0 ? 86 : 106)
+                .onAppear {
+                    DispatchQueue.global().async {
+                        homeViewModel.getTagList()
+                        homeViewModel.post()
+                    }
+                }
+                .onChange(of: isClose) { newValue in
+                    if newValue {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
                             homeViewModel.post()
-                        }
-                    }
-                    .onChange(of: isClose) { newValue in
-                        if newValue {
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                                homeViewModel.post()
-                                self.isClose = false
-                            }
+                            self.isClose = false
                         }
                     }
                 }

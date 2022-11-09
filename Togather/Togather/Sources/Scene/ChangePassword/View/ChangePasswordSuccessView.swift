10462d0bde57ef1cdcb12ed9482370fb94085ac0
BJCHO0501 <bjcho1503@naver.com>
2022-11-09 17:37:01 +0900

    📝 :: ChangePasswordSuccessView

--- Togather/Togather/Sources/Scene/ChangePassword/View/ChangePasswordSuccessView.swift
+++ Togather/Togather/Sources/Scene/ChangePassword/View/ChangePasswordSuccessView.swift
@@ -7,9 +7,12 @@ struct ChangePasswordSuccess: View {
     var body: some View {
         GeometryReader { proxy in
             VStack(alignment: .leading) {
-                Text("비밀번호가\n변경되었어요")
-                    .font(.custom("Pretendard-Bold", size: 32))
-                    .padding(.top, proxy.size.height / 5)
+                Spacer()
+                    .frame(height: 130)
+                ChangePasswordTitle(
+                    title: "비밀번호가\n변경되었어요",
+                    subTitle: ""
+                )
                 
                 GeometryReader { imageProxy in
                     Image("GreenCheck")
@@ -18,25 +21,13 @@ struct ChangePasswordSuccess: View {
                         .position(x: imageProxy.frame(in: .local).midX, y: imageProxy.frame(in: .local).midY)
                 }
                 
-                
-                Button(action: {
-                    goHome = true
-                }) {
-                    VStack(spacing: 0) {
-                        Rectangle()
-                            .frame(height: 0)
-                        
-                        Text("다음")
-                            .font(.custom("Pretendard-Bold", size: 18))
-                            .foregroundColor(.black)
+                ChangePasswordButton(
+                    isDisable: .constant(false),
+                    title: "다음",
+                    action: {
+                        goHome = true
                     }
-                    .padding(.vertical, 13)
-                    .background(Color(red: 0.882, green: 0.678, blue: 0.004))
-                    .cornerRadius(6)
-                    .padding(2)
-                    .background(Color(red: 0.7, green: 0.6, blue: 0.004))
-                    .cornerRadius(8)
-                }
+                )
                 .padding(.bottom, proxy.safeAreaInsets.bottom == 0 ? 30 : 5)
             }
             .padding(.horizontal, 16)

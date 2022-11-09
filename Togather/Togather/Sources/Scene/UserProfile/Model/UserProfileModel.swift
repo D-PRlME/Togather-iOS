1123025c017090dc6a0960a469226857f49c6229
BJCHO0501 <bjcho1503@naver.com>
2022-11-09 17:34:38 +0900

    📝 :: UserProfileModel

---
+++ Togather/Togather/Sources/Scene/UserProfile/Model/UserProfileModel.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+// MARK: - MyProfileModel (내 프로필 조회)
+struct UserProfileModel: Codable {
+    let name: String
+    let email: String
+    let profile_image_url: String
+    let introduce: String
+    let positions: [String]
+}
+
+/*
+ {
+     "name" : "김은빈",
+     "email" : "email@dsm.hs.kr",
+     "profile_image_url" : "http://",
+     "introduce":"백엔드 일짱임. 디코 아이디는 김은빈#1234",
+     "position":["BACKEND"]
+ }
+*/

d404b2140fe604af1653e7daab6024b1eec1729f
hsj._.06 <gtw030488@gmail.com>
2022-11-09 21:44:34 +0900

    📝 :: lint rules

--- Togather/Togather/Sources/Scene/MyView/View/myview ->/EditInfoView.swift
+++ Togather/Togather/Sources/Scene/MyView/View/myview ->/EditInfoView.swift
@@ -17,175 +17,173 @@ struct EditInfo: View {
     
     var body: some View {
         NavigationView {
-            GeometryReader { proxy in
-                ZStack {
-                    ColorManager.BackgroundColor.ignoresSafeArea()
-                    VStack(spacing: 0) {
-                        HStack {
-                            Spacer()
-                            Image("CloseBtn")
-                                .resizable()
-                                .frame(width: 30, height: 30)
-                                .padding(.top, 16)
-                                .onTapGesture {
-                                    self.presentationMode.wrappedValue.dismiss()
-                                }
-                        }
-                        HStack(spacing: 12) {
-                            KFImage.url(URL(string: changeMyInfoVM.profileImageLink))
-                                .placeholder {
-                                    Circle().fill(Color.whiteElevated3)
-                                        .frame(width: 48, height: 48)
-                                }
-                                .resizable()
-                                .clipShape(Circle())
-                                .frame(width: 48, height: 48)
-                                .overlay(Circle().stroke().foregroundColor(.whiteElevated3))
-                            
-                            PostButton(
-                                title: "이미지 변경",
-                                backgroundColor: .main,
-                                cornerColor: .mainDarken,
-                                action: {
-                                    self.showImagePicker.toggle()
-                                }
-                            )
-                            .sheet(isPresented: $showImagePicker) {
-                                ImagePicker(image: $changeMyInfoVM.image)
+            ZStack {
+                ColorManager.BackgroundColor.ignoresSafeArea()
+                VStack(spacing: 0) {
+                    HStack {
+                        Spacer()
+                        Image("CloseBtn")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .padding(.top, 16)
+                            .onTapGesture {
+                                self.presentationMode.wrappedValue.dismiss()
                             }
-                            .onChange(of: changeMyInfoVM.image) { _ in
-                                changeMyInfoVM.updateProfileImage()
+                    }
+                    HStack(spacing: 12) {
+                        KFImage.url(URL(string: changeMyInfoVM.profileImageLink))
+                            .placeholder {
+                                Circle().fill(Color.whiteElevated3)
+                                    .frame(width: 48, height: 48)
                             }
-                            Spacer()
-                        }
-                        .padding(.top, 26)
+                            .resizable()
+                            .clipShape(Circle())
+                            .frame(width: 48, height: 48)
+                            .overlay(Circle().stroke().foregroundColor(.whiteElevated3))
                         
-                        
-                        //ID
-                        EditInfoTextField(
-                            placeholder: "이름",
-                            text: $changeMyInfoVM.name
+                        PostButton(
+                            title: "이미지 변경",
+                            backgroundColor: .main,
+                            cornerColor: .mainDarken,
+                            action: {
+                                self.showImagePicker.toggle()
+                            }
                         )
-                        .padding(.top, 12)
-                        
-                        //Email
-                        VStack(alignment: .leading ,spacing: 0) {
-                            Rectangle()
-                                .frame(height: 0)
-                            
-                            Text(Account.email ?? "")
-                                .font(.title3m)
-                                .foregroundColor(.whiteElevated4)
-                                
+                        .sheet(isPresented: $showImagePicker) {
+                            ImagePicker(image: $changeMyInfoVM.image)
                         }
-                        .padding(12)
-                        .background(Color(red: 0.97, green: 0.97, blue: 0.97))
-                        .cornerRadius(6)
-                        .padding(1)
-                        .background(Color.whiteElevated3)
-                        .cornerRadius(6)
-                        .padding(.top, 8)
+                        .onChange(of: changeMyInfoVM.image) { _ in
+                            changeMyInfoVM.updateProfileImage()
+                        }
+                        Spacer()
+                    }
+                    .padding(.top, 26)
                     
+                    
+                    //ID
+                    EditInfoTextField(
+                        placeholder: "이름",
+                        text: $changeMyInfoVM.name
+                    )
+                    .padding(.top, 12)
+                    
+                    //Email
+                    VStack(alignment: .leading, spacing: 0) {
+                        Rectangle()
+                            .frame(height: 0)
                         
-                        //Introduce
-                        EditInfoTextEditor(
-                            text: $changeMyInfoVM.introduce,
-                            placeholder: "자기소계"
-                        )
-                        .frame(minHeight: 48, maxHeight: 48 * 3)
-                        .padding(.vertical, 8)
+                        Text(Account.email ?? "")
+                            .font(.title3m)
+                            .foregroundColor(.whiteElevated4)
                         
-                        VStack {
-                            HStack {
-                                Text("내 포지션")
-                                    .font(.indicator)
-                                    .foregroundColor(.whiteElevated4)
-                                
-                                Spacer()
-                            }
+                    }
+                    .padding(12)
+                    .background(Color(red: 0.97, green: 0.97, blue: 0.97))
+                    .cornerRadius(6)
+                    .padding(1)
+                    .background(Color.whiteElevated3)
+                    .cornerRadius(6)
+                    .padding(.top, 8)
+                    
+                    
+                    //Introduce
+                    EditInfoTextEditor(
+                        text: $changeMyInfoVM.introduce,
+                        placeholder: "자기소계"
+                    )
+                    .frame(minHeight: 48, maxHeight: 48 * 3)
+                    .padding(.vertical, 8)
+                    
+                    VStack {
+                        HStack {
+                            Text("내 포지션")
+                                .font(.indicator)
+                                .foregroundColor(.whiteElevated4)
                             
-                            FlowLayout(mode: .scrollable,
-                                       items: positionList,
-                                       itemSpacing: 2) { index in
-                                
-                                Text(index)
-                                    .foregroundColor(.text)
-                                    .font(.maintext2m)
-                                    .padding(.horizontal, 12)
-                                    .padding(.vertical, 6)
-                                    .background(changeMyInfoVM.positions.contains(index.PositionTypeChange) ? Color(red: 0.925, green: 0.792, blue: 0.357) : Color(red: 0.905, green: 0.905, blue: 0.905))
-                                    .cornerRadius(37)
-                                    .padding(1)
-                                    .background(changeMyInfoVM.positions.contains(index.PositionTypeChange) ? Color("YellowStroke") : Color(red: 0.153, green: 0.153, blue: 0.153, opacity: 0.15))
-                                    .cornerRadius(37)
-                                    .onTapGesture {
-                                        if changeMyInfoVM.positions.contains(index.PositionTypeChange) {
-                                            changeMyInfoVM.positions.removeAll { $0 == index.PositionTypeChange }
-                                        } else {
-                                            changeMyInfoVM.positions.append(index.PositionTypeChange)
-                                        }
-                                    }
-                            }
+                            Spacer()
                         }
+                        
+                        FlowLayout(mode: .scrollable,
+                                   items: positionList,
+                                   itemSpacing: 2) { index in
+                            
+                            Text(index)
+                                .foregroundColor(.text)
+                                .font(.maintext2m)
+                                .padding(.horizontal, 12)
+                                .padding(.vertical, 6)
+                                .background(changeMyInfoVM.positions.contains(index.positionTypeChange) ? Color(red: 0.925, green: 0.792, blue: 0.357) : Color(red: 0.905, green: 0.905, blue: 0.905))
+                                .cornerRadius(37)
+                                .padding(1)
+                                .background(changeMyInfoVM.positions.contains(index.positionTypeChange) ? Color("YellowStroke") : Color(red: 0.153, green: 0.153, blue: 0.153, opacity: 0.15))
+                                .cornerRadius(37)
+                                .onTapGesture {
+                                    if changeMyInfoVM.positions.contains(index.positionTypeChange) {
+                                        changeMyInfoVM.positions.removeAll { $0 == index.positionTypeChange }
+                                    } else {
+                                        changeMyInfoVM.positions.append(index.positionTypeChange)
+                                    }
+                                }
+                        }
+                    }
+                    
+                    Spacer()
+                    HStack {
+                        PostButton(
+                            title: "저장",
+                            backgroundColor: .main,
+                            cornerColor: .mainDarken,
+                            action: {
+                                if changeMyInfoVM.introduce.count <= introduceTextLimit {
+                                    changeMyInfoVM.changeMyInfo()
+                                } else {
+                                    alertMessage = "소개 글자수는 100자 이하입니다."
+                                    showAlert = true
+                                }
+                            }
+                        )
                         
                         Spacer()
-                        HStack {
-                            PostButton(
-                                title: "저장",
-                                backgroundColor: .main,
-                                cornerColor: .mainDarken,
-                                action: {
-                                    if (changeMyInfoVM.introduce.count <= introduceTextLimit) {
-                                        changeMyInfoVM.changeMyInfo()
-                                    } else {
-                                        alertMessage = "소개 글자수는 100자 이하입니다."
-                                        showAlert = true
-                                    }
-                                }
-                            )
-                            
-                            Spacer()
-                            NavigationLink(destination: DeleteAccount(goSignView: $goBack)) {
-                                Text("계정 삭제")
-                                    .foregroundColor(.text)
-                                    .font(.maintext1b)
-                                    .padding(.horizontal, 16)
-                                    .padding(.vertical, 8)
-                                    .background(Color.error)
-                                    .cornerRadius(37)
-                                    .padding(2)
-                                    .background(Color.redDarken)
-                                    .cornerRadius(37)
-                            }
-                            .buttonStyle(.plain)
-                            .alert("안내", isPresented: $showAlert) {
-                                Button("확인", role: .cancel) { }
-                            } message: {
-                                Text(alertMessage)
-                            }
+                        NavigationLink(destination: DeleteAccount(goSignView: $goBack)) {
+                            Text("계정 삭제")
+                                .foregroundColor(.text)
+                                .font(.maintext1b)
+                                .padding(.horizontal, 16)
+                                .padding(.vertical, 8)
+                                .background(Color.error)
+                                .cornerRadius(37)
+                                .padding(2)
+                                .background(Color.redDarken)
+                                .cornerRadius(37)
                         }
-                        .padding(.bottom, 17)
-                    }
-                    .padding(.horizontal, 16)
-                    .onAppear {
-                        if goBack {
-                            self.presentationMode.wrappedValue.dismiss()
-                        } else {
-                            changeMyInfoVM.getMyInfo()
+                        .buttonStyle(.plain)
+                        .alert("안내", isPresented: $showAlert) {
+                            Button("확인", role: .cancel) { }
+                        } message: {
+                            Text(alertMessage)
                         }
                     }
-                    .fullScreenCover(isPresented: $goBack) {
-                        NavigationView {
-                            LoginView()
-                        }
+                    .padding(.bottom, 17)
+                }
+                .padding(.horizontal, 16)
+                .onAppear {
+                    if goBack {
+                        self.presentationMode.wrappedValue.dismiss()
+                    } else {
+                        changeMyInfoVM.getMyInfo()
                     }
-                    .alert("안내", isPresented: $changeMyInfoVM.showingAlert) {
-                        Button("확인", role: .cancel) {
-                            self.presentationMode.wrappedValue.dismiss()
-                        }
-                    } message: {
-                        Text("프로필을 수정하였습니다.")
+                }
+                .fullScreenCover(isPresented: $goBack) {
+                    NavigationView {
+                        LoginView()
                     }
+                }
+                .alert("안내", isPresented: $changeMyInfoVM.showingAlert) {
+                    Button("확인", role: .cancel) {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }
+                } message: {
+                    Text("프로필을 수정하였습니다.")
                 }
             }
             .navigationBarHidden(true)

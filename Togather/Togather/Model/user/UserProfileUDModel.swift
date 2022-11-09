c4d1f3201285318360a35e0b3e0324abdd024783
hsj._.06 <gtw030488@gmail.com>
2022-11-09 21:45:51 +0900

    📝 :: lint rules

--- Togather/Togather/Model/user/UserProfileUDModel.swift
+++ Togather/Togather/Model/user/UserProfileUDModel.swift
@@ -1,67 +1,67 @@
 import Foundation
 
 struct Account {
-    static var _email: String?
+    static var localEmail: String?
     static var email: String? {
         get {
-            _email = UserDefaults.standard.string(forKey: "Account.User.Email")
-            return _email
+            localEmail = UserDefaults.standard.string(forKey: "Account.User.Email")
+            return localEmail
         }
         
         set(newEmail) {
             UserDefaults.standard.set(newEmail, forKey: "Account.User.Email")
-            _email = newEmail
+            localEmail = newEmail
         }
     }
     
-    static var _ID: String?
-    static var ID: String? {
+    static var localID: String?
+    static var identifier: String? {
         get {
-            _ID = UserDefaults.standard.string(forKey: "Account.User.ID")
-            return _ID
+            localID = UserDefaults.standard.string(forKey: "Account.User.ID")
+            return localID
         }
         
         set(newID) {
             UserDefaults.standard.set(newID, forKey: "Account.User.ID")
-            _ID = newID
+            localID = newID
         }
     }
     
-    static var _profileImageLink: String?
+    static var localProfileImageLink: String?
     static var profileImageLink: String? {
         get {
-            _profileImageLink = UserDefaults.standard.string(forKey: "Account.User.ProfileImagLink")
-            return _profileImageLink
+            localProfileImageLink = UserDefaults.standard.string(forKey: "Account.User.ProfileImagLink")
+            return localProfileImageLink
         }
         
         set(newProfileImagLink) {
             UserDefaults.standard.set(newProfileImagLink, forKey: "Account.User.ProfileImagLink")
-            _profileImageLink = newProfileImagLink
+            localProfileImageLink = newProfileImagLink
         }
     }
     
-    static var _positions: [String]?
+    static var localPositions: [String]?
     static var positions: [String]? {
         get {
-            _positions = UserDefaults.standard.stringArray(forKey: "Account.User.Positions")
-            return _positions
+            localPositions = UserDefaults.standard.stringArray(forKey: "Account.User.Positions")
+            return localPositions
         }
         
         set(newPositions) {
             UserDefaults.standard.set(newPositions, forKey: "Account.User.Positions")
-            _positions = newPositions
+            localPositions = newPositions
         }
     }
     
     static func deleteAccount() {
-        self.ID = nil
+        self.identifier = nil
         self.email = nil
         self.profileImageLink = nil
         self.positions = nil
     }
     
     static func setAccount(id: String?, email: String?, profileImagLink: String?, positions: [String]?) {
-        self.ID = id
+        self.identifier = id
         self.email = email
         self.profileImageLink = profileImagLink
         self.positions = positions

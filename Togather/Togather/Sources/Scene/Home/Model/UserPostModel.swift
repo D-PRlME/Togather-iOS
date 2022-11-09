79ea1bc47f06bff5226d2edf1ff112a80a5e0c3f
BJCHO0501 <bjcho1503@naver.com>
2022-11-09 17:35:12 +0900

    📝 :: UserPostModel

--- Togather/Togather/Sources/Scene/Home/Model/UserPostModel.swift
+++ Togather/Togather/Sources/Scene/Home/Model/UserPostModel.swift
@@ -1,8 +1,3 @@
-// This file was generated from JSON Schema using quicktype, do not modify it directly.
-// To parse the JSON, add this file to your project and do:
-//
-//   let userPostModel = try? newJSONDecoder().decode(UserPostModel.self, from: jsonData)
-
 import Foundation
 
 // MARK: - UserPostModel

12dbbac66d6edb234f5623a298822251c9874551
BJCHO0501 <bjcho1503@naver.com>
2022-11-11 23:10:15 +0900

    📝  :: SocketViewModel
    소켓에 사용할 ViewModel

--- Togather/Togather/Soket/ViewModel/SocketViewModel.swift
+++ Togather/Togather/Soket/ViewModel/SocketViewModel.swift
@@ -5,20 +5,25 @@ import Moya
 final class SocketViewModel: ObservableObject {
     
     private var manager = SocketManager(
-        socketURL: URL(string: "http://44.209.75.36:8081/socket.io")!,
+        socketURL: URL(string: "http://52.55.240.35:8081")!,
         config: [.log(true),
-            .compress,
-            .extraHeaders(Header.socket.header()!),
-            .version(.two)])
+        .compress,
+//        .extraHeaders(Header.socket.header()!),
+//        .version(.two),
+        .reconnects(false),
+//        .forceWebsockets(false)
+        ]
+    )
     
     var socket: SocketIOClient!
     @Published var message = [String]()
     
     init() {
-        socket = self.manager.defaultSocket
+        socket = self.manager.socket(forNamespace: "/socket.io")
         socket.on(clientEvent: .connect) { (data, ack) in
             print("서버에 연결되었습니다. \(data), \(ack)")
         }
+        socket.connect()
     }
     
     func socketCounnect() {

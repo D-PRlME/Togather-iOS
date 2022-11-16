eacfad8df1b2f209ba2f3499a149d53524feeac3
hsj._.06 <gtw030488@gmail.com>
2022-11-16 23:05:59 +0900

    ✨ :: lint 수정

--- Togather/Togather/ViewModels/user/Logout/LogoutViewModel.swift
+++ Togather/Togather/ViewModels/user/Logout/LogoutViewModel.swift
@@ -2,10 +2,8 @@ import Foundation
 import Moya
 
 class LogoutViewModel: ObservableObject {
-    let userClient = MoyaProvider<UserService>(plugins: [MoyaLoggerPlugin()])
-    
+    let userClient = MoyaProvider<UserService>(plugins: [MoyaLoggerPlugin()]
     @Published var isSucced: Bool = false
-    
     func logout() {
         userClient.request(.logout) { res in
             switch res {

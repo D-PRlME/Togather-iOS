9836ca0960739f852edfbba0e5cecfe983a9d9f2
hsj._.06 <gtw030488@gmail.com>
2022-11-16 23:06:31 +0900

    ✨ :: lint 수정

--- Togather/Togather/ViewModels/post/DeletePostViewModel.swift
+++ Togather/Togather/ViewModels/post/DeletePostViewModel.swift
@@ -1,13 +1,1 @@
-import Foundation
-import Moya
-import SwiftUI
-
-class DeletePostViewModel: ObservableObject {
-    let postClient = MoyaProvider<PostService>(plugins: [NetworkLoggerPlugin(configuration: .init(logOptions: .verbose))])
-    
-    @Published var postID: Int = 0
-    
-    
-}
-
 

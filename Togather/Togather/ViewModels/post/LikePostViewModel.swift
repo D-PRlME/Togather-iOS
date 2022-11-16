6d72ddb6dfb7d474722f2cef52060d22f5da5737
hsj._.06 <gtw030488@gmail.com>
2022-11-16 23:06:28 +0900

    ✨ :: lint 수정

--- Togather/Togather/ViewModels/post/LikePostViewModel.swift
+++ Togather/Togather/ViewModels/post/LikePostViewModel.swift
@@ -1,13 +1,1 @@
-import Foundation
-import Moya
-import SwiftUI
-
-class LikePostViewModel: ObservableObject {
-    let postClient = MoyaProvider<PostService>(plugins: [NetworkLoggerPlugin(configuration: .init(logOptions: .verbose))])
-    
-    @Published var postID: CLong = 0
-    
-    
-}
-
 

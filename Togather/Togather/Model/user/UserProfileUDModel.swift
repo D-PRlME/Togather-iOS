156997dacbef6cbd7b47c90092990d9233edf076
hsj._.06 <gtw030488@gmail.com>
2022-11-17 10:57:25 +0900

    ✨ :: 기존 Userdefault -> Keychain

--- Togather/Togather/Model/user/UserProfileUDModel.swift
+++ Togather/Togather/Model/user/UserProfileUDModel.swift
@@ -1,36 +1,37 @@
 import Foundation
+import SwiftKeychainWrapper
 
 struct Account {
     static var localEmail: String?
     static var email: String? {
         get {
-            localEmail = UserDefaults.standard.string(forKey: "Account.User.Email")
+            localEmail = KeychainWrapper.standard.string(forKey: "Account.User.Email")
             return localEmail
         }
         set(newEmail) {
-            UserDefaults.standard.set(newEmail, forKey: "Account.User.Email")
+            KeychainWrapper.standard.set(newEmail ?? "nil", forKey: "Account.User.Email")
             localEmail = newEmail
         }
     }
     static var localID: String?
     static var identifier: String? {
         get {
-            localID = UserDefaults.standard.string(forKey: "Account.User.ID")
+            localID = KeychainWrapper.standard.string(forKey: "Account.User.ID")
             return localID
         }
         set(newID) {
-            UserDefaults.standard.set(newID, forKey: "Account.User.ID")
+            KeychainWrapper.standard.set(newID ?? "nil", forKey: "Account.User.ID")
             localID = newID
         }
     }
     static var localProfileImageLink: String?
     static var profileImageLink: String? {
         get {
-            localProfileImageLink = UserDefaults.standard.string(forKey: "Account.User.ProfileImagLink")
+            localProfileImageLink = KeychainWrapper.standard.string(forKey: "Account.User.ProfileImagLink")
             return localProfileImageLink
         }
         set(newProfileImagLink) {
-            UserDefaults.standard.set(newProfileImagLink, forKey: "Account.User.ProfileImagLink")
+            KeychainWrapper.standard.set(newProfileImagLink ?? "nil", forKey: "Account.User.ProfileImagLink")
             localProfileImageLink = newProfileImagLink
         }
     }

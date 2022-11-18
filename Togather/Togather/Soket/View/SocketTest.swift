abbf213762af0471561974d64ecd2b4bcac7d4e0
hsj._.06 <gtw030488@gmail.com>
2022-11-18 16:22:23 +0900

    📝 :: SocektTest

--- Togather/Togather/Soket/View/SocketTest.swift
+++ Togather/Togather/Soket/View/SocketTest.swift
@@ -1,35 +1,35 @@
 import SwiftUI
 
 struct SocketTest: View {
-    @StateObject var socketVM = SocketViewModel()
+    @StateObject var socketViewModel = SocketViewModel()
     var body: some View {
         VStack {
             Button {
-                socketVM.socketCounnect()
+                socketViewModel.socketCounnect()
             } label: {
                 Text("소켓 on")
                     .frame(width: 100, height: 100)
             }
             Button {
-                socketVM.socketDisconnect()
+                socketViewModel.socketDisconnect()
             } label: {
                 Text("소켓 off")
                     .frame(width: 100, height: 100)
             }
             Button {
-                socketVM.onError()
+                socketViewModel.onError()
             } label: {
                 Text("소켓 error")
                     .frame(width: 100, height: 100)
             }
             Button {
-                socketVM.onChat()
+                socketViewModel.onChat()
             } label: {
                 Text("소켓 chat")
                     .frame(width: 100, height: 100)
             }
             Button {
-                socketVM.onRoom()
+                socketViewModel.onRoom()
             } label: {
                 Text("소켓 room")
                     .frame(width: 100, height: 100)

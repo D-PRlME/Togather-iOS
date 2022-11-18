afb3df109d98a6ba7edbb263105a9b9c4697a675
hsj._.06 <gtw030488@gmail.com>
2022-11-18 16:21:49 +0900

    📝 :: ChangePasswordModel

--- Togather/Togather/Sources/Scene/ChangePassword/Model/ChangePasswordModel.swift
+++ Togather/Togather/Sources/Scene/ChangePassword/Model/ChangePasswordModel.swift
@@ -2,11 +2,16 @@ import Foundation
 
 // MARK: - MyProfileModel (내 프로필 조회)
 struct ChangePasswordModel: Codable {
-    let name: String
-    let email: String
-    let profileImageUrl: String
-    let introduce: String
+    let userID: Int
+    let name, email, profileImageURL, introduce: String
     let positions: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case userID = "user_id"
+        case name, email
+        case profileImageURL = "profile_image_url"
+        case introduce, positions
+    }
 }
 
 /*

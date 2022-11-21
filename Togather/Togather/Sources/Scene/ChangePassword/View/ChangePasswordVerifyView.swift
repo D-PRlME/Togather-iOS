b43aabb4204885de47a55ccaff7cc6547282def2
hsj._.06 <gtw030488@gmail.com>
2022-11-21 22:52:51 +0900

    📝 :: ChangePasswordVerifyView

--- Togather/Togather/Sources/Scene/ChangePassword/View/ChangePasswordVerifyView.swift
+++ Togather/Togather/Sources/Scene/ChangePassword/View/ChangePasswordVerifyView.swift
@@ -18,7 +18,7 @@ struct ChangePasswordVerifyView: View {
                         .frame(height: 130)
                     ChangePasswordTitle(
                         title: "비밀번호 번경",
-                        subTitle: "계정에 입력된 이메일로 전송된 6자리 인증 번호를 입력해 주세요. 인증 번호는 \(changePasswordVerifyViewModel.timer)초 후에 만료됩니다"
+                        subTitle: "계정에 입력된 이메일로 전송된 6자리 인증 번호를 입력해 주세요.\n인증 번호는 \(changePasswordVerifyViewModel.timer)초 후에 만료됩니다"
                     )
                     ChangePasswordTextField(
                         placeholder: "인증 번호",

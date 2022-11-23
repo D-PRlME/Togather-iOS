2d41f37a7e44bb33f0f87583791ed101dd62fb23
BJCHO0501 <bjcho1503@naver.com>
2022-11-23 23:03:44 +0900

    📝 :: ChatListView

--- Togather/Togather/Sources/Scene/Chat/View/ChatListView.swift
+++ Togather/Togather/Sources/Scene/Chat/View/ChatListView.swift
@@ -4,20 +4,22 @@ import Kingfisher
 struct ChatListView: View {
     @StateObject var chatListViewModel = ChatListViewModel()
     @State private var gotoChattingView: Bool = false
-    @State private var count: Int = 0
+    @State private var selectRoomID: Int = 0
+    @State private var selectRoomName: String = ""
     var body: some View {
         GeometryReader { proxy in
             ZStack {
                 ColorManager.BackgroundColor.ignoresSafeArea()
                 VStack(spacing: 0) {
                     ScrollView {
-                        ForEach(0..<chatListViewModel.chattingList.count, id: \.self) { index in
+                        ForEach(0..<chatListViewModel.chattingRoomList.count, id: \.self) { index in
                             Button {
                                 gotoChattingView = true
-                                count = index
+                                selectRoomID = chatListViewModel.chattingRoomList[index].roomID
+                                selectRoomName = chatListViewModel.chattingRoomList[index].roomName
                             } label: {
                                 HStack(spacing: 0) {
-                                    KFImage.url(URL(string: chatListViewModel.chattingList[index].roomLogoImage))
+                                    KFImage.url(URL(string: chatListViewModel.chattingRoomList[index].roomLogoImage))
                                         .placeholder {
                                             Circle()
                                                 .fill(Color.secondary)
@@ -32,10 +34,10 @@ struct ChatListView: View {
                                         .overlay(Circle().stroke().foregroundColor(Color("TabBarStroke")))
                                         .padding(8)
                                     VStack(alignment: .leading, spacing: 0) {
-                                        Text(chatListViewModel.chattingList[index].roomName)
+                                        Text(chatListViewModel.chattingRoomList[index].roomName)
                                             .foregroundColor(.black)
                                             .font(.custom("Pretendard-Bold", size: 20))
-                                        Text(chatListViewModel.chattingList[index].lastChat.lastMessage)
+                                        Text(chatListViewModel.chattingRoomList[index].lastChat.lastMessage)
                                             .foregroundColor(.black)
                                             .font(.custom("Pretendard-Medium", size: 18))
                                             .lineLimit(1)
@@ -46,7 +48,10 @@ struct ChatListView: View {
                                 .frame(width: proxy.size.width - 32)
                                 .cornerRadius(8)
                                 .fullScreenCover(isPresented: $gotoChattingView) {
-                                    ChattingView(index: $count)
+                                    ChattingView(
+                                        index: $selectRoomID,
+                                        roomName: $selectRoomName
+                                    )
                                 }
                             }
                         }

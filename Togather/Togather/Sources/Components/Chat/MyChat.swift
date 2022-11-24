705767d996600ab83037568a23f36d2e5b0267ba
BJCHO0501 <bjcho1503@naver.com>
2022-11-24 18:59:07 +0900

    📝 :: MyChat

--- Togather/Togather/Sources/Components/Chat/MyChat.swift
+++ Togather/Togather/Sources/Components/Chat/MyChat.swift
@@ -2,19 +2,23 @@ import SwiftUI
 
 struct MyChat: View {
     let content: String
+    let date: String
     var body: some View {
-        HStack {
+        HStack(alignment: .bottom, spacing: 4) {
             Spacer()
+            Text(date)
+                .foregroundColor(Color.whiteElevated4)
+                .font(.indicator)
             Text(content)
                 .multilineTextAlignment(.leading)
                 .foregroundColor(.black)
-                .font(.custom("Pretendard-Medium", size: 18))
+                .font(.maintext1m)
                 .padding(.horizontal, 16)
                 .padding(.vertical, 8)
-                .background(Color("main-Elevated"))
+                .background(Color.mainElevated)
                 .cornerRadius(24)
                 .padding(1)
-                .background(Color("main-Darken"))
+                .background(Color.mainDarken)
                 .cornerRadius(24)
         }
     }
@@ -22,6 +26,6 @@ struct MyChat: View {
 
 struct MyChat_Previews: PreviewProvider {
     static var previews: some View {
-        MyChat(content: "에베베베베")
+        MyChat(content: "에베베베베", date: "오전 11:12")
     }
 }

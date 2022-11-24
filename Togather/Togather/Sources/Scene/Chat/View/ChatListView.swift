90c2294540edbb0330b38659bae81694fde9094f
BJCHO0501 <bjcho1503@naver.com>
2022-11-24 18:58:40 +0900

    📝 :: ChatListView

--- Togather/Togather/Sources/Scene/Chat/View/ChatListView.swift
+++ Togather/Togather/Sources/Scene/Chat/View/ChatListView.swift
@@ -13,46 +13,65 @@ struct ChatListView: View {
                 VStack(spacing: 0) {
                     ScrollView {
                         ForEach(0..<chatListViewModel.chattingRoomList.count, id: \.self) { index in
-                            Button {
-                                gotoChattingView = true
-                                selectRoomID = chatListViewModel.chattingRoomList[index].roomID
-                                selectRoomName = chatListViewModel.chattingRoomList[index].roomName
-                            } label: {
-                                HStack(spacing: 0) {
-                                    KFImage.url(URL(string: chatListViewModel.chattingRoomList[index].roomLogoImage))
-                                        .placeholder {
-                                            Circle()
-                                                .fill(Color.secondary)
-                                                .opacity(0.1)
+                            ZStack(alignment: .topTrailing) {
+                                Button {
+                                    gotoChattingView = true
+                                    selectRoomID = chatListViewModel.chattingRoomList[index].roomID
+                                    selectRoomName = chatListViewModel.chattingRoomList[index].roomName
+                                } label: {
+                                    HStack(spacing: 0) {
+                                        KFImage.url(URL(string: chatListViewModel.chattingRoomList[index].roomLogoImage))
+                                            .placeholder {
+                                                Circle()
+                                                    .fill(Color.secondary)
+                                                    .opacity(0.1)
+                                            }
+                                            .resizable()
+                                            .scaledToFit()
+                                            .clipShape(Circle())
+                                            .frame(width: 56, height: 56)
+                                            .cornerRadius(20)
+                                            .padding(1)
+                                            .overlay(
+                                                Circle()
+                                                    .stroke()
+                                                    .foregroundColor(.whiteElevated3)
+                                            )
+                                            .padding(8)
+                                        VStack(alignment: .leading, spacing: 0) {
+                                            Text(chatListViewModel.chattingRoomList[index].roomName)
+                                                .foregroundColor(.black)
+                                                .font(.title3b)
+                                            Text(chatListViewModel.chattingRoomList[index].lastChat.lastMessage)
+                                                .foregroundColor(chatListViewModel.chattingRoomList[index].isRead ? .whiteElevated4 : .text)
+                                                .font(.maintext1m)
+                                                .lineLimit(1)
                                         }
-                                        .resizable()
-                                        .scaledToFit()
-                                        .clipShape(Circle())
-                                        .frame(width: 56, height: 56)
-                                        .cornerRadius(20)
-                                        .padding(1)
-                                        .overlay(Circle().stroke().foregroundColor(Color("TabBarStroke")))
-                                        .padding(8)
-                                    VStack(alignment: .leading, spacing: 0) {
-                                        Text(chatListViewModel.chattingRoomList[index].roomName)
-                                            .foregroundColor(.black)
-                                            .font(.custom("Pretendard-Bold", size: 20))
-                                        Text(chatListViewModel.chattingRoomList[index].lastChat.lastMessage)
-                                            .foregroundColor(.black)
-                                            .font(.custom("Pretendard-Medium", size: 18))
-                                            .lineLimit(1)
+                                        Spacer()
                                     }
-                                    Spacer()
+                                    .background(Color.whiteElevated1)
+                                    .frame(width: proxy.size.width - 32)
+                                    .cornerRadius(8)
+                                    .fullScreenCover(isPresented: $gotoChattingView) {
+                                        ChattingView(
+                                            index: $selectRoomID,
+                                            roomName: $selectRoomName
+                                        )
+                                    }
                                 }
-                                .background(Color("white-Elevated1"))
-                                .frame(width: proxy.size.width - 32)
-                                .cornerRadius(8)
-                                .fullScreenCover(isPresented: $gotoChattingView) {
-                                    ChattingView(
-                                        index: $selectRoomID,
-                                        roomName: $selectRoomName
-                                    )
+                                
+                                if !chatListViewModel.chattingRoomList[index].isRead {
+                                    Circle()
+                                        .frame(width: 16,height: 16)
+                                        .foregroundColor(.main)
+                                        .overlay(
+                                            Circle()
+                                                .frame(width: 6, height: 6)
+                                                .foregroundColor(.white)
+                                        )
+                                        .padding([.top, .trailing], 16)
                                 }
+                                
                             }
                         }
                     }

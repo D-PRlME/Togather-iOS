9f08770cf0296b02a85fe46ce3963c751c65242b
BJCHO0501 <bjcho1503@naver.com>
2022-12-02 17:50:28 +0900

    📝 :: WritingView

--- Togather/Togather/Sources/Scene/WritingPost/View/WritingView.swift
+++ Togather/Togather/Sources/Scene/WritingPost/View/WritingView.swift
@@ -7,73 +7,77 @@ struct WritingView: View {
     @State private var goTags: Bool = false
     @StateObject var writingViewModel = WritingViewModel()
     var body: some View {
-        VStack {
-            // MARK: - 취소버튼
-
-            HStack {
-                Spacer()
-                Image("CloseBtn")
-                    .resizable()
-                    .frame(width: 30, height: 30)
-                    .padding(.top, 16)
-                    .onTapGesture {
-                        self.showModal.toggle()
-                    }
-            }
-            // MARK: - 제목
-
-            PostTextField(
-                placeholder: "제목",
-                text: $writingViewModel.title
-            )
-            // MARK: - 본문
-
-            PostTextEditor(
-                text: $writingViewModel.content,
-                placeholder: "본문을 입력하세요"
-            )
-            FlowLayout(mode: .scrollable,
-                       items: tagBtnArr,
-                       itemSpacing: 2) { index in
-                Text(index)
-                    .foregroundColor(.white)
-                    .font(.maintext2m)
-                    .padding(.horizontal, 12)
-                    .padding(.vertical, 6)
-                    .background(Color.whiteElevated5)
-                    .cornerRadius(37)
-                    .padding(1)
-                    .background(Color.whiteElevated4)
-                    .cornerRadius(37)
-            }
-            // MARK: - 글쓰기 버튼
-            HStack {
-                PostButton(
-                    title: "글쓰기",
-                    backgroundColor: .main,
-                    cornerColor: .mainDarken,
-                    action: {
-                        writingViewModel.tag = self.tagBtnArr
-                        writingViewModel.post()
-                        showModal = false
-                    }
-                )
-                Spacer()
-                PostButton(
-                    title: "모든 태그 보기",
-                    backgroundColor: .whiteElevated1,
-                    cornerColor: .whiteElevated3,
-                    action: {
-                        goTags = true
-                    }
-                )
-                .sheet(isPresented: $goTags) {
-                    TagListView(goBack: $goTags, tagLists: $tagBtnArr)
+        BackgroundTapGesture {
+            VStack {
+                // MARK: - 취소버튼
+                HStack {
+                    Spacer()
+                    Image("CloseBtn")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .padding(.top, 16)
+                        .onTapGesture {
+                            self.showModal.toggle()
+                        }
                 }
-            }
-        } // Vstack
-        .padding(.horizontal, 16)
-        .padding(.bottom, 16)
+                // MARK: - 제목
+
+                PostTextField(
+                    placeholder: "제목",
+                    text: $writingViewModel.title
+                )
+                // MARK: - 본문
+
+                PostTextEditor(
+                    text: $writingViewModel.content,
+                    placeholder: "본문을 입력하세요"
+                )
+                FlowLayout(mode: .scrollable,
+                           items: tagBtnArr,
+                           itemSpacing: 2) { index in
+                    Text(index)
+                        .foregroundColor(.white)
+                        .font(.maintext2m)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 6)
+                        .background(Color.whiteElevated5)
+                        .cornerRadius(37)
+                        .padding(1)
+                        .background(Color.whiteElevated4)
+                        .cornerRadius(37)
+                }
+                // MARK: - 글쓰기 버튼
+                HStack {
+                    PostButton(
+                        title: "글쓰기",
+                        backgroundColor: .main,
+                        cornerColor: .mainDarken,
+                        action: {
+                            writingViewModel.tag = self.tagBtnArr
+                            writingViewModel.post()
+                            showModal = false
+                        }
+                    )
+                    Spacer()
+                    PostButton(
+                        title: "모든 태그 보기",
+                        backgroundColor: .whiteElevated1,
+                        cornerColor: .whiteElevated3,
+                        action: {
+                            goTags = true
+                        }
+                    )
+                    .sheet(isPresented: $goTags) {
+                        TagListView(goBack: $goTags, tagLists: $tagBtnArr)
+                    }
+                }
+            } // Vstack
+            .padding(.horizontal, 16)
+            .padding(.bottom, 16)
+        }
+        .onTapGesture {
+            UIApplication.shared.endEditing()
+        }
     } // body
 }
 

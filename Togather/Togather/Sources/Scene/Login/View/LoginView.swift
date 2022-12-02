dc47cd5bf2ac50338ca0f2dc966a351d095397d2
BJCHO0501 <bjcho1503@naver.com>
2022-12-02 17:51:57 +0900

    📝 :: LoginView

--- Togather/Togather/Sources/Scene/Login/View/LoginView.swift
+++ Togather/Togather/Sources/Scene/Login/View/LoginView.swift
@@ -5,36 +5,41 @@ struct LoginView: View {
     @State private var password = ""
     @StateObject var loginViewModel = LoginViewModel()
     var body: some View {
-        ZStack {
-            ColorManager.BackgroundColor.ignoresSafeArea()
-            NavigationLink(destination: TabBarView(), tag: 1, selection: $loginViewModel.viewTag ) { EmptyView() }
-            VStack(alignment: .leading, spacing: 12) {
-                Spacer()
-                LoginTitle()
-                LoginTextField(
-                    id: $loginViewModel.email,
-                    password: $loginViewModel.password
-                )
-                AuthButton(
-                    isLoading: $loginViewModel.showProgrees,
-                    text: "로그인",
-                    action: {
-                        loginViewModel.showProgrees = true
-                        loginViewModel.login()
-                    }
-                )
-                LoginNavigationButton()
-                Spacer()
-                LoginImageButton()
+        BackgroundTapGesture {
+            ZStack {
+                ColorManager.BackgroundColor.ignoresSafeArea()
+                NavigationLink(destination: TabBarView(), tag: 1, selection: $loginViewModel.viewTag ) { EmptyView() }
+                VStack(alignment: .leading, spacing: 12) {
+                    Spacer()
+                    LoginTitle()
+                    LoginTextField(
+                        id: $loginViewModel.email,
+                        password: $loginViewModel.password
+                    )
+                    AuthButton(
+                        isLoading: $loginViewModel.showProgrees,
+                        text: "로그인",
+                        action: {
+                            loginViewModel.showProgrees = true
+                            loginViewModel.login()
+                        }
+                    )
+                    LoginNavigationButton()
+                    Spacer()
+                    LoginImageButton()
+                }
             }
+            .padding([.horizontal, .bottom], 16)
+            .alert("안내", isPresented: $loginViewModel.showError) {
+                Button("확인", role: .cancel) { }
+            } message: {
+                Text(loginViewModel.errorMessage)
+            }
+            .navigationBarHidden(true)
         }
-        .padding([.horizontal, .bottom], 16)
-        .alert("안내", isPresented: $loginViewModel.showError) {
-            Button("확인", role: .cancel) { }
-        } message: {
-            Text(loginViewModel.errorMessage)
+        .onTapGesture {
+            UIApplication.shared.endEditing()
         }
-        .navigationBarHidden(true)
     }
 }
 

fb276f736cd3e7ad92ae05d7bfd011ccf152d53d
BJCHO0501 <bjcho1503@naver.com>
2022-12-02 17:50:21 +0900

    📝 :: SignUpView

--- Togather/Togather/Sources/Scene/SignUp/View/SignUpView.swift
+++ Togather/Togather/Sources/Scene/SignUp/View/SignUpView.swift
@@ -7,49 +7,54 @@ struct SignUpView: View {
     @State private var nameText: String = ""
     // MARK: - body
     var body: some View {
-        ZStack {
-            NavigationLink(
-                destination:
-                    SignUpEmailVerifyView(
+        BackgroundTapGesture {
+            ZStack {
+                NavigationLink(
+                    destination:
+                        SignUpEmailVerifyView(
+                            email: $emailText,
+                            password: $passwordText,
+                            name: $nameText
+                        ),
+                    tag: 1,
+                    selection: $signUpViewModel.isSuccess
+                ) { EmptyView() }
+                VStack(alignment: .leading, spacing: 0) {
+                    Spacer()
+                    SignUpTitle()
+                        .padding(.bottom, 50)
+                    // MARK: - textFileds
+                    SignUpTextField(
                         email: $emailText,
                         password: $passwordText,
-                        name: $nameText
-                    ),
-                tag: 1,
-                selection: $signUpViewModel.isSuccess
-            ) { EmptyView() }
-            VStack(alignment: .leading, spacing: 0) {
-                Spacer()
-                SignUpTitle()
-                    .padding(.bottom, 50)
-                // MARK: - textFileds
-                SignUpTextField(
-                    email: $emailText,
-                    password: $passwordText,
-                    name: $nameText,
-                    emailError: .constant(!signUpViewModel.emailValueCheck(emailText)),
-                    passwordError: .constant(!signUpViewModel.passwordValueCheck(passwordText))
-                )
-                Spacer()
-                // MARK: - 다음 버튼
-                AuthButton(
-                    isDisable: .constant(!signUpViewModel.checkTotalSignUpValue(emailText, passwordText, nameText)),
-                    text: "다음",
-                    action: {
-                        signUpViewModel.email = emailText
-                        signUpViewModel.emailDuplicate()
-                    }
-                )
-            } // Vstack
-            .padding(.horizontal, 16)
-            .padding(.bottom, 16)
-            BackBtn()
+                        name: $nameText,
+                        emailError: .constant(!signUpViewModel.emailValueCheck(emailText)),
+                        passwordError: .constant(!signUpViewModel.passwordValueCheck(passwordText))
+                    )
+                    Spacer()
+                    // MARK: - 다음 버튼
+                    AuthButton(
+                        isDisable: .constant(!signUpViewModel.checkTotalSignUpValue(emailText, passwordText, nameText)),
+                        text: "다음",
+                        action: {
+                            signUpViewModel.email = emailText
+                            signUpViewModel.emailDuplicate()
+                        }
+                    )
+                } // Vstack
+                .padding(.horizontal, 16)
+                .padding(.bottom, 16)
+                BackBtn()
+            }
+            .navigationBarHidden(true)
+            .alert("안내", isPresented: $signUpViewModel.showError) {
+                Button("확인", role: .cancel) { }
+            } message: {
+                Text(signUpViewModel.errorMessage)
+            }
         }
-        .navigationBarHidden(true)
-        .alert("안내", isPresented: $signUpViewModel.showError) {
-            Button("확인", role: .cancel) { }
-        } message: {
-            Text(signUpViewModel.errorMessage)
+        .onTapGesture {
+            UIApplication.shared.endEditing()
         }
     } // body
 }

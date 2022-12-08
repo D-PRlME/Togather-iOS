ddbc5aafa09832a5aee0bb1fb92a090eb43df456
hsj._.06 <gtw030488@gmail.com>
2022-12-08 22:54:22 +0900

    ✨ :: ContentTextEditor

--- Togather/Togather/Sources/Components/ContentTextEditor.swift
+++ Togather/Togather/Sources/Components/ContentTextEditor.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-struct EditInfoTextEditor: View {
+struct ContentTextEditor: View {
     @Binding var text: String
     let placeholder: String
     init(
@@ -17,40 +17,54 @@ struct EditInfoTextEditor: View {
     }
     var body: some View {
         if #available(iOS 16.0, *) {
-            TextEditor(text: $text)
-                .autocapitalization(.none)
-                .disableAutocorrection(true)
-                .scrollContentBackground(.hidden)
-                .font(.title3m)
-                .padding(.leading, 8)
-                .background(Color.whiteElevated1)
-                .cornerRadius(6)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 6)
-                        .stroke(lineWidth: 1)
-                        .foregroundColor(.whiteElevated3)
-                )
-                .multilineTextAlignment(.leading)
+            ZStack(alignment: .topLeading) {
+                if text.isEmpty {
+                    Text(placeholder)
+                        .foregroundColor(.whiteElevated4)
+                        .padding(.leading, 5)
+                        .padding(.top, 8)
+                }
+                TextEditor(text: $text)
+                    .scrollContentBackground(.hidden)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+            }
+            .font(.title3m)
+            .padding(8)
+            .background(Color.whiteElevated1)
+            .cornerRadius(6)
+            .overlay(
+                RoundedRectangle(cornerRadius: 6)
+                    .stroke(lineWidth: 1)
+                    .foregroundColor(.whiteElevated3)
+            )
+            .multilineTextAlignment(.leading)
         } else {
-            TextEditor(text: $text)
-                .autocapitalization(.none)
-                .disableAutocorrection(true)
-                .font(.title3m)
-                .padding(.leading, 8)
-                .background(Color.whiteElevated1)
-                .cornerRadius(6)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 6)
-                        .stroke(lineWidth: 1)
-                        .foregroundColor(.whiteElevated3)
-                )
-                .multilineTextAlignment(.leading)
+            ZStack(alignment: .leading) {
+                if text.isEmpty {
+                    Text(placeholder)
+                        .foregroundColor(.gray)
+                }
+                TextEditor(text: $text)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+            }
+            .font(.title3m)
+            .padding(8)
+            .background(Color.whiteElevated1)
+            .cornerRadius(6)
+            .overlay(
+                RoundedRectangle(cornerRadius: 6)
+                    .stroke(lineWidth: 1)
+                    .foregroundColor(.whiteElevated3)
+            )
+            .multilineTextAlignment(.leading)
         }
     }
 }
 
 struct EditInfoTextEditor_Previews: PreviewProvider {
     static var previews: some View {
-        EditInfoTextEditor(text: .constant("안녕하세요"), placeholder: "dsf")
+        ContentTextEditor(text: .constant(""), placeholder: "자기소개")
     }
 }
